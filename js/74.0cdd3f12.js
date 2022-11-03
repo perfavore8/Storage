a820798f6cff75eaff07fc85c7662e0f82c33511
perfavore8 <nekit2353@mail.ru>
2022-11-03 10:51:21 +0200

    preparing for deploy

---
+++ js/74.0cdd3f12.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkstorage"]=self["webpackChunkstorage"]||[]).push([[74],{6035:function(n,t,e){e.r(t),e.d(t,{default:function(){return f}});var r=e(3396);function u(n,t,e,u,c,o){const s=(0,r.up)("document-setting");return(0,r.wg)(),(0,r.j4)(s)}var c=e(16),o={components:{DocumentSetting:c.Z}},s=e(89);const a=(0,s.Z)(o,[["render",u]]);var f=a}}]);
+//# sourceMappingURL=74.0cdd3f12.js.map
