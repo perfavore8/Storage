bd65080c0376afd7fcea8b50dc09bd74abd0bcb4
perfavore8 <nekit2353@mail.ru>
2022-11-09 15:49:24 +0200

    productProperties add styles

--- src/components/ProductProperties.vue
+++ src/components/ProductProperties.vue
@@ -10,14 +10,18 @@
             </btns-save-close>
           </div>
           <br />
-          <div>
-            <button
+          <div class="links">
+            <div
+              class="triangle"
+              :class="{
+                triangle_last: selected_fields_properties.length - 1 == idx,
+              }"
               v-for="(cat, idx) in selected_fields_properties"
-              :key="cat.name"
+              :key="cat.id"
               @click="prev_cat(idx)"
             >
               {{ cat.name }}
-            </button>
+            </div>
           </div>
           <div class="bottom">
             <SelectorVue
@@ -712,6 +716,92 @@ export default {
   .btn2:hover {
     background-color: #0256d4;
     box-shadow: 0 0 5px 2px rgb(2 86 212 / 25%);
+  }
+}
+.links {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  margin-bottom: 10px;
+  $step: 12px;
+
+  .triangle {
+    cursor: pointer;
+    padding: 5px 10px 5px 15px;
+    height: 20px;
+    margin: 5px 0;
+    @include font(400, 15px);
+    -webkit-clip-path: polygon(
+      0 0,
+      calc(100% - $step) 0,
+      100% 50%,
+      calc(100% - $step) 100%,
+      0% 100%,
+      $step 50%
+    );
+    clip-path: polygon(
+      0 0,
+      calc(100% - $step) 0,
+      100% 50%,
+      calc(100% - $step) 100%,
+      0% 100%,
+      $step 50%
+    );
+    display: flex;
+    justify-content: start;
+    align-items: center;
+    color: #000;
+  }
+  .triangle:hover {
+    padding: 5px 12px 5px 17px;
+    margin-left: -2px;
+    margin-right: -2px;
+    background: #e6e6e6 !important;
+  }
+  .triangle:first-child:hover {
+    margin-left: 0;
+  }
+  .triangle_last {
+    color: #fff;
+    background: rgba(27, 53, 70, 0.9) !important;
+  }
+  .triangle_last:hover {
+    background: rgba(27, 53, 70, 0.8) !important;
+  }
+  .triangle:active {
+    background: #d6d6d6 !important;
+  }
+
+  .triangle:nth-child(3n + 1) {
+    background: linear-gradient(45deg, hsl(0, 0%, 90%), hsl(0, 0%, 84%));
+  }
+  .triangle:nth-child(3n + 2) {
+    background: linear-gradient(
+      45deg,
+      hsl(0, 0%, 84%),
+      hsl(0, 0%, 83%),
+      hsl(0, 0%, 84%)
+    );
+  }
+  .triangle:nth-child(3n + 3) {
+    background: linear-gradient(45deg, hsl(0, 0%, 84%), hsl(0, 0%, 90%));
+  }
+  .triangle:first-child {
+    padding-left: 5px;
+    -webkit-clip-path: polygon(
+      0 0,
+      calc(100% - $step) 0,
+      100% 50%,
+      calc(100% - $step) 100%,
+      0% 100%
+    );
+    clip-path: polygon(
+      0 0,
+      calc(100% - $step) 0,
+      100% 50%,
+      calc(100% - $step) 100%,
+      0% 100%
+    );
   }
 }
 </style>
