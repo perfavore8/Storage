899f30dad127d75e710568c39e44ece7186baad1
perfavore8 <nekit2353@mail.ru>
2022-11-18 14:09:49 +0200

    some refactoring

--- src/components/ProductProperties.vue
+++ src/components/ProductProperties.vue
@@ -754,9 +754,6 @@ export default {
   background-color: #0b76ef;
   background-image: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 8 8'%3e%3cpath fill='%23fff' d='M6.564.75l-3.59 3.612-1.538-1.55L0 4.26 2.974 7.25 8 2.193z'/%3e%3c/svg%3e");
 }
-.checkbox:not(:disabled):not(:checked) + label:hover::before {
-  // border-color: #b3d7ff;
-}
 .checkbox:not(:disabled):active + label::before {
   background-color: #b3d7ff;
   border-color: #b3d7ff;
--- src/store/index.js
+++ src/store/index.js
@@ -3,7 +3,8 @@ import data from "./modules/data";
 import shows from "./modules/shows";
 import fields from "./modules/fields";
 import categories from "./modules/categories";
+import products from "./modules/products";
 
 export default createStore({
-  modules: { data, shows, fields, categories },
+  modules: { data, shows, fields, categories, products },
 });
