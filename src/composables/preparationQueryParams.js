77d17774537ab47c77b118d3a6157bac0383c993
perfavore8 <nekit2353@mail.ru>
2022-11-18 13:11:01 +0200

    some refactoring

--- src/composables/preparationQueryParams.js
+++ src/composables/preparationQueryParams.js
@@ -3,10 +3,7 @@ export function usePreparationQueryParams() {
     let str = "";
     Object.entries(params).forEach((val, idx) => {
       idx === 0 ? (str += "?") : (str += "&");
-      console.log(val[1], typeof val[1], val[1]?.length > 0);
-      let a = "";
-      typeof val[1] == "object" ? (a = "[" + val[1] + "]") : (a = val[1]);
-      str += val[0] + "=" + a;
+      str += val[0] + "=" + val[1];
     });
     return str;
   };
