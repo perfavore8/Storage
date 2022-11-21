1846de9e028a2d0144f3337ab7c2286963cc85bb
perfavore8 <nekit2353@mail.ru>
2022-11-21 15:26:40 +0200

    same refactoring

--- src/components/MainGridFilters.vue
+++ src/components/MainGridFilters.vue
@@ -1,50 +1,50 @@
 <template>
-  <tr class="filters" v-show="show_filter">
-    <transition-group name="mdl">
-      <transition name="mdl" key="1b">
+  <transition name="mdl1">
+    <tr class="filters" v-show="show_filter">
+      <transition-group name="mdl">
         <th class="item" key="b1"></th>
-      </transition>
-      <th
-        class="item"
-        v-show="fields[idx].table_config.visible"
-        v-for="(filter, idx) in filtersValue"
-        :key="idx"
-      >
-        <filter-number
-          v-if="filter.type == 1 || filter.type == 2"
-          :item="filter"
-          :idx="idx"
-          @change_filter_value="change_filter_value"
-        />
-        <filter-text
-          v-if="filter.type == 3 || filter.type == 4"
-          :item="filter"
-          :idx="idx"
-          @change_filter_value="change_filter_value"
-        />
-        <filter-list
-          v-if="filter.type == 5 || filter.type == 6"
-          :item="filter"
-          :idx="idx"
-          :selector_options="filter.selector_options"
-          @change_filter_value="change_filter_value"
-        />
-        <filter-date
-          v-if="filter.type == 7 || filter.type == 8"
-          :item="filter"
-          :idx="idx"
-          @change_filter_value="change_filter_value"
-        />
-        <filter-flag
-          v-if="filter.type == 9"
-          :item="filter"
-          :idx="idx"
-          @change_filter_value="change_filter_value"
-        />
-      </th>
-      <th class="item" key="2b" />
-    </transition-group>
-  </tr>
+        <th
+          class="item"
+          v-show="fields[idx].table_config.visible && show_filter"
+          v-for="(filter, idx) in filtersValue"
+          :key="idx"
+        >
+          <filter-number
+            v-if="filter.type == 1 || filter.type == 2"
+            :item="filter"
+            :idx="idx"
+            @change_filter_value="change_filter_value"
+          />
+          <filter-text
+            v-if="filter.type == 3 || filter.type == 4"
+            :item="filter"
+            :idx="idx"
+            @change_filter_value="change_filter_value"
+          />
+          <filter-list
+            v-if="filter.type == 5 || filter.type == 6"
+            :item="filter"
+            :idx="idx"
+            :selector_options="filter.selector_options"
+            @change_filter_value="change_filter_value"
+          />
+          <filter-date
+            v-if="filter.type == 7 || filter.type == 8"
+            :item="filter"
+            :idx="idx"
+            @change_filter_value="change_filter_value"
+          />
+          <filter-flag
+            v-if="filter.type == 9"
+            :item="filter"
+            :idx="idx"
+            @change_filter_value="change_filter_value"
+          />
+        </th>
+        <th class="item" key="2b" />
+      </transition-group>
+    </tr>
+  </transition>
 </template>
 
 <script>
@@ -137,5 +137,13 @@ export default {
 .mdl-leave-to {
   opacity: 0;
   transform: scale(0.5, 0.5);
+}
+.mdl1-enter-active,
+.mdl1-leave-active {
+  transition: all 0.15s ease-out;
+}
+.mdl1-enter-from,
+.mdl1-leave-to {
+  opacity: 0;
 }
 </style>
