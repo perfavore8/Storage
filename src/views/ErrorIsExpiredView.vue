37a4023df3b87677cedc76c13ea1fcd879062d93
perfavore8 <nekit2353@mail.ru>
2022-12-14 23:08:08 +0200

    errors pages add all

---
+++ src/views/ErrorIsExpiredView.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="view">
+    <h1>Внимание!</h1>
+    <p>
+      Срок подписки виджета Sales-Склад подошел к концу. Для продления
+      обратитесь в отдел продаж: +7 (495) 278-18-15 или sales@sls-up.ru
+    </p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+@import "@/app.scss";
+.view {
+  height: 100%;
+  width: 100%;
+  min-height: 100vh;
+  min-width: 100vw;
+  display: flex;
+  flex-direction: column;
+  user-select: none;
+  text-align: center;
+}
+h1 {
+  @include font(500, 2rem);
+  margin: 80px auto 0;
+}
+p {
+  margin: 12px auto 0;
+  width: 40%;
+  color: #6c757d;
+  @include font(300, 1.25rem);
+}
+</style>
---
+++ src/views/ErrorIsNotInstalledView.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="view">
+    <h1>Склад не установен</h1>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+@import "@/app.scss";
+.view {
+  height: 100%;
+  width: 100%;
+  min-height: 100vh;
+  min-width: 100vw;
+  display: flex;
+  flex-direction: column;
+  user-select: none;
+  text-align: center;
+}
+h1 {
+  @include font(500, 2rem);
+  margin: 40px auto;
+}
+</style>
