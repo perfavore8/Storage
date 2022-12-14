64a4ac884cf59d9415292a6800fd6a889920c818
perfavore8 <nekit2353@mail.ru>
2022-12-14 22:55:14 +0200

    add 2 errors views and repath to them

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHashHistory } from "vue-router";
+import store from "../store";
 
 const routes = [
   {
@@ -32,12 +33,40 @@ const routes = [
     name: "genezis",
     component: () => import("../views/GenezisView.vue"),
   },
+  {
+    path: "/Error_is_not_installed",
+    name: "Error_is_not_installed",
+    component: () => import("../views/ErrorIsNotInstalledView.vue"),
+  },
+  {
+    path: "/Error_is_expired",
+    name: "Error_is_expired",
+    component: () => import("../views/ErrorIsExpiredView.vue"),
+  },
 ];
 
 const router = createRouter({
   // history: createWebHistory(process.env.BASE_URL),
   history: createWebHashHistory(),
   routes,
+});
+
+router.beforeEach(async (to, from, next) => {
+  await store.dispatch("get_account");
+  if (
+    !store.state.account?.account?.install &&
+    to.path != "/Error_is_not_installed"
+  )
+    next("/Error_is_not_installed");
+  if (
+    (store.state.account?.account?.install &&
+      to.path == "/Error_is_not_installed") ||
+    (!store.state.account?.account?.expired && to.path == "/Error_is_expired")
+  )
+    next("/");
+  if (!store.state.account?.account?.expired && to.path != "/Error_is_expired")
+    next("/Error_is_expired"); // не забудь переключить !expired => expired
+  else next();
 });
 
 export default router;
