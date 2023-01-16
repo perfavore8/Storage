3ffcb44b651dcd8016a3b948e7e907cb14881e70
perfavore8 <nekit2353@mail.ru>
2023-01-16 16:01:42 +0200

    some refactoring

--- src/components/BtnsSaveClose.vue
+++ src/components/BtnsSaveClose.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="btns">
-    <button class="btn btn1" @click="close()" v-if="show_close">
+    <button class="btn btn_grey" @click="close()" v-if="show_close">
       <slot name="close">Отмена</slot>
     </button>
     <slot name="other_btns"></slot>
-    <button class="btn btn2" @click="save()" v-if="show_save">
+    <button class="btn btn_blue" @click="save()" v-if="show_save">
       <slot name="save">Сохранить</slot>
     </button>
   </div>
@@ -46,31 +46,5 @@ export default {
   display: flex;
   flex-direction: row;
   gap: 10px;
-
-  .btn {
-    cursor: pointer;
-    padding: 6px 12px;
-    height: 36px;
-    border: none;
-    @include font(400, 16px);
-    border-radius: 5px;
-    transition: background-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
-  }
-  .btn1 {
-    color: #fff;
-    background-color: #6c757d;
-  }
-  .btn1:hover {
-    background-color: #5f676d;
-    box-shadow: 0 0 5px 2px rgb(95 103 109 / 25%);
-  }
-  .btn2 {
-    color: #fff;
-    background-color: #0d6efd;
-  }
-  .btn2:hover {
-    background-color: #0256d4;
-    box-shadow: 0 0 5px 2px rgb(2 86 212 / 25%);
-  }
 }
 </style>
