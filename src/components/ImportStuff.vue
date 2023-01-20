205ec2426b24f59c7c88bb8e7a1cbbebbbee9602
perfavore8 <nekit2353@mail.ru>
2023-01-20 13:35:30 +0200

    display grid

--- src/components/ImportStuff.vue
+++ src/components/ImportStuff.vue
@@ -3,16 +3,25 @@
     <div class="backdrop" @click="close()" />
   </Teleport>
   <div class="wrapper">
-    <header>
+    <header class="header">
       <h1>Настройки импорта</h1>
       <button class="btn cross" @click="close()"></button>
     </header>
-    <main>
+    <main class="main">
       <div class="template">
-        <SelectorVue :options_props="[]" :selected_option="{}" />
-        <input class="input" type="text" />
+        <SelectorVue
+          :options_props="templates.list"
+          :selected_option="templates.selected"
+          @select="selectTempate"
+        />
+        <input
+          class="input"
+          type="text"
+          v-model="templates.newTemplateName"
+          v-if="templates.selected.showTemplateName"
+        />
       </div>
-      <table class="table">
+      <!-- <table class="table">
         <thead>
           <tr class="row title">
             <td class="item" v-for="i in 10" :key="i">h{{ i }}</td>
@@ -27,13 +36,40 @@
           </tr>
           <tr class="row">
             <td class="item" v-for="i in 10" :key="i">
-              <SelectorVue :options_props="[]" :selected_option="{}" />
+              <SelectorVue
+                :options_props="[
+                  { name: '1', value: 1 },
+                  { name: '2', value: 2 },
+                  { name: '3', value: 3 },
+                  { name: '4', value: 4 },
+                ]"
+                :selected_option="{ name: '1', value: 1 }"
+              />
             </td>
           </tr>
         </tbody>
-      </table>
+      </table> -->
+      <div class="table">
+        <div class="item title" v-for="i in gridCount" :key="i">h{{ i }}</div>
+
+        <div class="item" v-for="i in gridCount" :key="i">i{{ i }}</div>
+
+        <div class="item" v-for="i in gridCount" :key="i">i{{ i }}</div>
+
+        <div class="item" v-for="i in gridCount" :key="i">
+          <SelectorVue
+            :options_props="[
+              { name: '1', value: 1 },
+              { name: '2', value: 2 },
+              { name: '3', value: 3 },
+              { name: '4', value: 4 },
+            ]"
+            :selected_option="{ name: '1', value: 1 }"
+          />
+        </div>
+      </div>
     </main>
-    <footer>
+    <footer class="footer">
       <BtnsSaveClose @close="close" />
     </footer>
   </div>
@@ -43,11 +79,42 @@
 import store from "@/store";
 import BtnsSaveClose from "./BtnsSaveClose.vue";
 import SelectorVue from "./SelectorVue.vue";
+import { reactive } from "@vue/reactivity";
 export default {
   components: { SelectorVue, BtnsSaveClose },
   setup() {
+    const gridCount = 10;
+    const templates = reactive({
+      newTemplateName: "",
+      selected: {},
+      list: [
+        {
+          name: "Без шаблона",
+          value: 0,
+          showTemplateName: false,
+        },
+        {
+          name: "Новый шаблон",
+          value: 1,
+          showTemplateName: true,
+        },
+        {
+          name: "Шаблон 1",
+          value: 2,
+          showTemplateName: false,
+        },
+      ],
+    });
+
+    templates.selected = templates.list[0];
+
+    const selectTempate = (option) => (templates.selected = option);
+
     const close = () => store.commit("openCloseImportStuff");
     return {
+      gridCount,
+      templates,
+      selectTempate,
       close,
     };
   },
@@ -77,7 +144,7 @@ export default {
   text-align: left;
   display: flex;
   flex-direction: column;
-  header {
+  .header {
     display: flex;
     justify-content: space-between;
     align-items: center;
@@ -99,8 +166,8 @@ export default {
       transform: rotate(90deg);
     }
   }
-  main {
-    padding: 12px 32px;
+  .main {
+    padding: 32px 32px;
     display: flex;
     flex-direction: column;
     gap: 16px;
@@ -108,23 +175,25 @@ export default {
       display: flex;
       flex-direction: row;
       gap: 16px;
+      .input {
+        width: 50%;
+      }
     }
     .table {
       border-collapse: collapse;
       width: 100%;
-      display: block;
+      display: grid;
+      grid-template-rows: auto auto auto auto;
+      // grid-template-columns: auto auto auto auto auto auto auto auto auto auto;
+      grid-template-columns: repeat(v-bind(gridCount), auto);
       // max-width: 100vw;
-      overflow: scroll;
+      overflow-x: scroll;
+      overflow-y: visible;
       .title {
         @include font(700, 16px);
         background-color: rgba(0, 0, 0, 0.1) !important;
-        .item {
-          padding-bottom: 20px !important;
-          text-align: center !important;
-        }
-      }
-      .row_odd {
-        background-color: rgba(0, 0, 0, 0.05);
+        padding-bottom: 20px !important;
+        text-align: center !important;
       }
       .item {
         padding: 12px 16px;
@@ -137,7 +206,7 @@ export default {
       }
     }
   }
-  footer {
+  .footer {
     border-top: 2px solid #dee2e6;
     padding: 12px 32px;
     display: flex;
