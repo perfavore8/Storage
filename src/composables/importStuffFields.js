80bde947b1e84b929789801c29c8c81f8b1d77e9
perfavore8 <nekit2353@mail.ru>
2023-01-26 14:25:52 +0200

    add useImportStuffFields

---
+++ src/composables/importStuffFields.js
@@ -0,0 +1,37 @@
+import store from "@/store";
+import { reactive } from "vue";
+
+export function useImportStuffFields() {
+  const selectedImportStuffFields = reactive([]);
+
+  const importStuffFields = reactive([]);
+
+  const getFields = async () => {
+    importStuffFields.push(
+      { name: "Не импортировать поле", value: -1 },
+      { name: "Создать новое поле", value: 0 }
+    );
+    await store.dispatch("get_types");
+    const types = store.state.fields.types;
+    const fields = Object.entries(await store.dispatch("importStuffFields"));
+    fields.forEach((field) => {
+      importStuffFields.push({ name: types[field[0]], value: "optgroup" });
+      field[1].map(
+        (item) => ((item.value = item.code), (item.optgroup = true))
+      );
+      importStuffFields.push(...field[1]);
+    });
+  };
+  getFields();
+
+  const fillSelectedImportStuffFields = (count) => {
+    for (let i = 0; i < count; i++)
+      selectedImportStuffFields.push(importStuffFields[0]);
+  };
+
+  return {
+    selectedImportStuffFields,
+    importStuffFields,
+    fillSelectedImportStuffFields,
+  };
+}
