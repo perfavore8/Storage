58cb0d39bf65b98427f0362a84fcfecf0385740d
perfavore8 <nekit2353@mail.ru>
2023-02-07 17:13:08 +0200

    add divide setting

---
+++ src/components/AccountSattings.vue
@@ -0,0 +1,163 @@
+<template>
+  <div class="backdrop" @click="close()" />
+  <div class="wrapper">
+    <div class="container">
+      <div class="header">
+        <label>Настройки аккаунта</label>
+        <button class="btn_del" @click="close()">
+          <div class="icon"></div>
+        </button>
+      </div>
+      <div class="content">
+        <div class="block">
+          <h6>Этапы резервирования товаров со склада</h6>
+          <div class="text">
+            <input
+              type="checkbox"
+              class="checkbox"
+              v-model="divideRight"
+              id="asd1"
+            />
+            <label for="asd1">Разделять права</label>
+          </div>
+        </div>
+      </div>
+      <div class="footer">
+        <BtnsSaveClose @save="save" @close="close" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import BtnsSaveClose from "./BtnsSaveClose.vue";
+import { computed, onMounted, ref } from "@vue/runtime-core";
+import store from "@/store";
+export default {
+  components: { BtnsSaveClose },
+  setup() {
+    const account = computed(() => {
+      return store.state.account.account;
+    });
+
+    const divideRight = ref(false);
+    onMounted(() => {
+      if (account.value?.config?.divideRight)
+        divideRight.value = account.value?.config?.divideRight;
+    });
+
+    const save = () => {
+      store.dispatch("update_account", { divideRight: divideRight.value });
+      close();
+    };
+
+    const close = () => {
+      store.commit("openCloseAccountSattings", false);
+    };
+
+    return { divideRight, account, close, save };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@/app.scss";
+.wrapper {
+  pointer-events: all;
+  width: 100%;
+  height: max-content;
+  min-height: 100vh;
+  position: absolute;
+  top: 0;
+  left: 0;
+  background: transparent;
+  @include font(400, 16px);
+  .container {
+    position: relative;
+    z-index: 800;
+    max-width: 1140px;
+    background-color: #fff;
+    background-clip: padding-box;
+    border: 1px solid rgba(0, 0, 0, 0.2);
+    border-radius: 0.3rem;
+    margin: 30px auto;
+    outline: 0;
+    box-shadow: 0 0 7px 6px rgb(206 212 218 / 50%);
+    text-align: left;
+    display: flex;
+    flex-direction: column;
+    .header {
+      display: flex;
+      justify-content: space-between;
+      padding: 10px 50px;
+      @include font(500, 20px);
+      border-bottom: 2px solid #dee2e6;
+      .btn_del {
+        background: transparent;
+        border-radius: 0 5px 5px 0;
+        height: 30px;
+        width: 30px;
+        margin: 0 auto;
+        cursor: pointer;
+        border: none;
+        border-radius: 4px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        transition: background-color 0.15s ease-in-out,
+          box-shadow 0.15s ease-in-out;
+        margin: 0;
+        margin-right: -10px;
+        .icon {
+          width: inherit;
+          height: inherit;
+          transition: transform 0.15s ease-in-out;
+          @include bg_image("@/assets/cross_black.svg", 100% 100%);
+        }
+        .icon:hover {
+          transform: rotate(90deg);
+        }
+      }
+    }
+    .content {
+      @include font(400, 16px);
+      padding: 15px 50px;
+      border-bottom: 2px solid #dee2e6;
+      display: flex;
+      flex-direction: row;
+      gap: 20px;
+      .block {
+        display: flex;
+        flex-direction: column;
+        width: 100%;
+
+        h6 {
+          @include font(500, 16px);
+        }
+
+        .text {
+          display: flex;
+          flex-direction: column;
+          border: 1px solid #c9c9c9;
+          border-radius: 4px;
+          padding: 16px;
+        }
+      }
+    }
+    .footer {
+      display: flex;
+      justify-content: end;
+      padding: 15px 50px;
+    }
+  }
+}
+.backdrop {
+  background-color: transparent;
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  z-index: 799;
+}
+</style>
