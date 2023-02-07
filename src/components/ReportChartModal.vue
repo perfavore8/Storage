4e67012c2589770929d7840fbd08916ee13c3670
perfavore8 <nekit2353@mail.ru>
2023-02-07 16:13:01 +0200

    add line

--- src/components/ReportChartModal.vue
+++ src/components/ReportChartModal.vue
@@ -11,6 +11,12 @@
           :propsData="ChartsDoughnutCompanies"
           :title="ChartsDoughnutCompanies.name"
         />
+        <ChartsLine
+          v-if="ChartsLineMounth.datasets.length"
+          :propsData="ChartsLineMounth"
+          :title="ChartsLineMounth.name"
+          :propsOptions="ChartsLineMounth.propsOptions"
+        />
       </div>
     </div>
   </teleport>
@@ -19,9 +25,11 @@
 <script>
 import { useColor } from "@/composables/color";
 import ChartsDoughnut from "@/components/Charts/ChartsDoughnut.vue";
+import ChartsLine from "@/components/Charts/ChartsLine.vue";
 import { reactive } from "@vue/reactivity";
+import store from "@/store";
 export default {
-  components: { ChartsDoughnut },
+  components: { ChartsDoughnut, ChartsLine },
   props: {
     reportsData: Object,
     total: Object,
@@ -87,9 +95,138 @@ export default {
     });
     ChartsDoughnutCompanies.fillData();
 
+    const mounths = reactive([
+      {
+        name: "Март",
+        data: [],
+        date: "03/01/2022-03/31/2022",
+      },
+      {
+        name: "Апрель",
+        data: [],
+        date: "04/01/2022-04/30/2022",
+      },
+      {
+        name: "Май",
+        data: [],
+        date: "05/01/2022-05/31/2022",
+      },
+      {
+        name: "Июнь",
+        data: [],
+        date: "06/01/2022-06/30/2022",
+      },
+      {
+        name: "Июль",
+        data: [],
+        date: "07/01/2022-07/31/2022",
+      },
+      {
+        name: "Август",
+        data: [],
+        date: "08/01/2022-08/31/2022",
+      },
+      {
+        name: "Сентябрь",
+        data: [],
+        date: "09/01/2022-09/30/2022",
+      },
+      {
+        name: "Октябрь",
+        data: [],
+        date: "10/01/2022-10/31/2022",
+      },
+      {
+        name: "Ноябрь",
+        data: [],
+        date: "11/01/2022-11/30/2022",
+      },
+      {
+        name: "Декабрь",
+        data: [],
+        date: "11/01/2022-11/31/2022",
+      },
+      {
+        name: "Январь",
+        data: [],
+        date: "01/01/2023-01/31/2023",
+      },
+      {
+        name: "Февраль",
+        data: [],
+        date: "02/01/2023-02/28/2023",
+      },
+    ]);
+
+    mounths.map(async (mounth, idx) => {
+      const data = await store.dispatch("getCustomersTotal", {
+        filter: { date: mounth.date },
+        page: 1, // FIXME временно <----
+      });
+      data.sum === null ? (data.sum = 0) : null;
+      data.cost_sum === null ? (data.cost_sum = 0) : null;
+      data.prib = data.sum - data.cost_sum;
+      mounths[idx].data = data;
+    });
+
+    const ChartsLineMounth = reactive({
+      dataCode: [
+        { code: "sum", name: "Оборот", color: "" },
+        { code: "prib", name: "Прибыль", color: "" },
+      ],
+      titleCode: "company",
+      name: "Отчет по месяцам",
+      labels: [],
+      datasets: [],
+      propsOptions: {
+        scales: {
+          y: {
+            // min: 0,
+            // max: 200,
+            // step: 100,
+            suggestedMin: 50,
+            suggestedMax: 100,
+          },
+        },
+        animations: {
+          tension: {
+            duration: 1000,
+            easing: "linear",
+            from: 0.2,
+            to: 0.5,
+          },
+        },
+      },
+      fillData: () => {
+        ChartsLineMounth.dataCode.map((val) => {
+          val.color = getRandomColor();
+        });
+        mounths.forEach((mounth) => ChartsLineMounth.labels.push(mounth.name));
+        ChartsLineMounth.dataCode.forEach((code) => {
+          const dataset = {
+            label: code.name,
+            backgroundColor: code.color,
+            data: [],
+          };
+          mounths.forEach((mounth) => {
+            dataset.data.push(mounth.data[code.code]);
+          });
+          ChartsLineMounth.datasets.push(dataset);
+        });
+      },
+    });
+    setTimeout(() => {
+      ChartsLineMounth.fillData();
+    }, 2000);
+
     const close = () => context.emit("close");
 
-    return { ChartsDoughnutMoney, ChartsDoughnutCompanies, close };
+    return {
+      ChartsDoughnutMoney,
+      ChartsDoughnutCompanies,
+      ChartsLineMounth,
+      close,
+    };
   },
 };
 </script>
