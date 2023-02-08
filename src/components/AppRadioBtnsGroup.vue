7f318b93b0c688567959e075676d04bb6e6f3dbc
perfavore8 <nekit2353@mail.ru>
2023-02-08 15:37:48 +0200

    add AppRadioBtnsGroup

---
+++ src/components/AppRadioBtnsGroup.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="btns_group">
+    <button
+      class="btn"
+      :class="{ selected: selected.value === item.value }"
+      v-for="item in list"
+      :key="item.value"
+      @click="select(item)"
+    >
+      {{ item.name }}
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    list: Array,
+    selected: Object,
+  },
+  setup(props, context) {
+    const select = (val) => context.emit("select", val);
+
+    return { select };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@/app.scss";
+.btns_group {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  border-radius: 6px;
+  padding: 8px;
+  background-color: #e5e7eb;
+  .btn {
+    white-space: nowrap;
+    border-radius: 6px;
+    background-color: transparent;
+    color: rgb(75, 85, 99);
+  }
+  .btn:hover {
+    color: rgb(31, 41, 55);
+  }
+  .selected {
+    color: hsl(209, 62%, 40%);
+    background-color: white;
+    padding-left: 24px;
+    padding-right: 24px;
+    box-shadow: 0 1px 3px 0 rgb(0 0 0 / 10%), 0 1px 2px 0 rgb(0 0 0 / 6%);
+  }
+  .selected:hover {
+    color: hsl(209, 62%, 40%);
+  }
+}
+</style>
