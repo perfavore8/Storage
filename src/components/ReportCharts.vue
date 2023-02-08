835e0361a9d5cd834d4e4b0e151f2ed700639cbb
perfavore8 <nekit2353@mail.ru>
2023-02-08 16:19:56 +0200

    add

---
+++ src/components/ReportCharts.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="wrapper">
+    <ChartsBar :data="bar.data" :options="bar.options" :title="bar.title" />
+  </div>
+</template>
+
+<script>
+import ChartsBar from "@/components/Charts/ChartsBar.vue";
+import { reactive } from "@vue/reactivity";
+export default {
+  components: { ChartsBar },
+  setup() {
+    const bar = reactive({
+      title: "",
+      data: {
+        labels: ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s"],
+        datasets: [
+          {
+            label: "Data One",
+            data: [40, 20, 10, 82, 73, 27, 23, 17, 52, 12, 54, 64, 56],
+            fill: false,
+            backgroundColor: "#5DADE2",
+          },
+        ],
+      },
+      options: {
+        responsive: true,
+      },
+    });
+
+    return { bar };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.wrapper {
+  width: 80%;
+  margin: 0 auto;
+}
+</style>
