ea08a3ab2b00b62eee864562b79ef28e3c2bb9cd
perfavore8 <nekit2353@mail.ru>
2023-02-10 16:04:19 +0200

    done

---
+++ src/components/NotificationCard.vue
@@ -0,0 +1,106 @@
+<template>
+  <div class="card">
+    <span class="logo info material-icons-round" v-if="notification.type == 0">
+      info
+    </span>
+    <span
+      class="logo success material-icons-round"
+      v-if="notification.type == 1"
+    >
+      check_circle
+    </span>
+    <span
+      class="logo warning material-icons-round"
+      v-if="notification.type == 2"
+    >
+      warning
+    </span>
+    <span class="logo error material-icons-round" v-if="notification.type == 3">
+      error
+    </span>
+    <div class="main">
+      <span class="header">{{ notification.header }}</span>
+      <span class="text">{{ notification.text }}</span>
+    </div>
+    <button class="cross btn" @click="close()">
+      <span class="material-icons-outlined"> close </span>
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    notification: Object,
+  },
+  emits: { close },
+  setup(props, context) {
+    const close = () => context.emit("close");
+    return { close };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@/app.scss";
+* {
+  box-sizing: border-box;
+}
+.card {
+  position: relative;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  border-radius: 12px;
+  background: #fff;
+  padding: 20px 35px 20px 25px;
+  box-shadow: 0 6px 20px -5px rgb(0 0 0 / 20%);
+  overflow: hidden;
+  width: fit-content;
+  transition: all 0.5s cubic-bezier(0.68, -0.55, 0.265, 1.35);
+  .logo {
+    font-size: 32px;
+  }
+  .main {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    margin: 0 20px;
+    .header {
+      @include font(600, 16px);
+      color: #333;
+    }
+    .text {
+      color: #666;
+    }
+  }
+  .cross {
+    position: absolute;
+    top: 10px;
+    right: 15px;
+    padding: 5px;
+    opacity: 0.7;
+    padding: 0;
+    background-color: transparent;
+    > span {
+      font-size: 18px;
+    }
+  }
+}
+.success {
+  color: #10b981;
+  // background-color: #caebd9;
+}
+.info {
+  color: #4070f4;
+  // background-color: #cae1fc;
+}
+.warning {
+  color: #f59e0b;
+  // background-color: #ffe0c9;
+}
+.error {
+  color: #ef4444;
+  // background-color: #f3d6d8;
+}
+</style>
---
+++ src/components/NotificationCenter.vue
@@ -0,0 +1,104 @@
+<template>
+  <div class="center">
+    <template v-for="(notification, idx) in notificationsData" :key="idx">
+      <transition name="card">
+        <NotificationCard
+          v-if="notification.show"
+          :notification="{
+            type: notification.type,
+            header: notification.header,
+            text: notification.text,
+          }"
+          @close="() => stop(idx)"
+        />
+      </transition>
+    </template>
+  </div>
+  <button @click="start()" :disabled="inProgress">123</button>
+</template>
+
+<script>
+import { reactive } from "@vue/reactivity";
+import NotificationCard from "./NotificationCard.vue";
+import { computed } from "@vue/runtime-core";
+export default {
+  components: { NotificationCard },
+  setup() {
+    const notificationsData = reactive([
+      {
+        type: 0,
+        header: "Notification info",
+        text: "Lorem ipsum dolor",
+        show: false,
+        timer: null,
+      },
+      {
+        type: 1,
+        header: "Notification success",
+        text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
+        show: false,
+        timer: null,
+      },
+      {
+        type: 2,
+        header: "Notification warning",
+        text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Distinctio animi libero vel",
+        show: false,
+        timer: null,
+      },
+      {
+        type: 3,
+        header: "Notification error",
+        text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Distinctio animi libero vel, quas neque tenetur ipsum voluptatum!",
+        show: false,
+        timer: null,
+      },
+    ]);
+
+    const delay = 8000;
+
+    const start = () => {
+      notificationsData.forEach((val, idx) => {
+        setTimeout(() => {
+          val.show = true;
+          val.timer = setTimeout(() => (val.show = false), delay);
+        }, idx * 400);
+      });
+    };
+
+    const inProgress = computed(() =>
+      notificationsData.some((val) => val.show)
+    );
+
+    const stop = (idx) => {
+      notificationsData[idx].show = false;
+      notificationsData[idx].timer = null;
+    };
+
+    return { notificationsData, stop, start, inProgress };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@/app.scss";
+.center {
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  margin: 16px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 8px;
+}
+.card-enter-active,
+.card-leave-active {
+  transition: all 0.3s ease;
+}
+.card-enter-from,
+.card-leave-to {
+  opacity: 0.5;
+  transform: translateX(calc(-100% - 46px));
+}
+</style>
