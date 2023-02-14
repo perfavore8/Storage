ee8d530d4e3d406f5fdee514baa54fec318f841e
perfavore8 <nekit2353@mail.ru>
2023-02-14 12:18:42 +0200

    add VueApexCharts

--- src/main.js
+++ src/main.js
@@ -3,4 +3,5 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import "material-icons/iconfont/material-icons.css";
-createApp(App).use(store).use(router).mount("#app");
+import VueApexCharts from "vue3-apexcharts";
+createApp(App).use(store).use(router).use(VueApexCharts).mount("#app");
