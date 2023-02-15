52f142571d070b27b1ec75e3cb7adb57af5c2af2
perfavore8 <nekit2353@mail.ru>
2023-02-15 10:48:32 +0200

    new charts

--- src/components/ReportCharts.vue
+++ src/components/ReportCharts.vue
@@ -1,64 +1,146 @@
 <template>
   <div class="wrapper">
-    <ChartsBar
+    <apexchart
       v-if="chartType === 'bar'"
-      :data="bar.data"
-      :options="bar.options"
-      :title="bar.title"
-    />
-    <ChartsDoughnut
+      :type="bar.type"
+      :width="clientWidth > 400 ? 600 : 300"
+      :options="bar.chartOptions"
+      :series="bar.series"
+    ></apexchart>
+    <apexchart
       v-else-if="chartType === 'doughnut'"
-      :data="doughnut.data"
-      :options="doughnut.options"
-      :title="doughnut.title"
-    />
+      :type="doughnut.type"
+      :width="clientWidth > 400 ? 600 : 300"
+      :options="doughnut.chartOptions"
+      :series="doughnut.series"
+    ></apexchart>
   </div>
 </template>
 
 <script>
-import ChartsBar from "@/components/Charts/ChartsBar.vue";
-import ChartsDoughnut from "@/components/Charts/ChartsDoughnut.vue";
 import { reactive } from "@vue/reactivity";
+import { computed } from "@vue/runtime-core";
 export default {
-  components: { ChartsBar, ChartsDoughnut },
+  components: {},
   props: {
     chartType: String,
   },
   setup() {
+    const clientWidth = computed(() => document.documentElement.clientWidth);
     const bar = reactive({
-      title: "",
-      data: {
-        labels: ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s"],
-        datasets: [
+      type: "bar",
+      series: [
+        {
+          name: "Free Cash Flow",
+          data: [35, 41, 36, 26, 45, 48, 52, 53, 41, 31, 44, 39],
+        },
+      ],
+      chartOptions: {
+        chart: {
+          type: "bar",
+        },
+        plotOptions: {
+          bar: {
+            horizontal: false,
+            columnWidth: "65%",
+            endingShape: "rounded",
+          },
+        },
+        responsive: [
           {
-            label: "Data One",
-            data: [40, 20, 10, 82, 73, 27, 23, 17, 52, 12, 54, 64, 56],
-            fill: false,
-            backgroundColor: "#5DADE2",
+            breakpoint: 880,
+            options: {
+              chart: {
+                width: 320,
+              },
+              xaxis: {
+                labels: {
+                  rotateAlways: true,
+                  show: true,
+                  rotate: -45,
+                },
+              },
+            },
           },
         ],
-      },
-      options: {
-        responsive: true,
-      },
-    });
-    const doughnut = reactive({
-      title: "",
-      data: {
-        labels: ["VueJs", "EmberJs", "ReactJs", "AngularJs"],
-        datasets: [
-          {
-            backgroundColor: ["#41B883", "#E46651", "#00D8FF", "#DD1B16"],
-            data: [40, 20, 80, 10],
+        dataLabels: {
+          enabled: false,
+        },
+        xaxis: {
+          categories: [
+            "Feb",
+            "Mar",
+            "Apr",
+            "May",
+            "Jun",
+            "Jul",
+            "Aug",
+            "Sep",
+            "Oct",
+            "Nov",
+            "Dec",
+            "Jen",
+          ],
+        },
+        yaxis: {
+          title: {
+            text: "",
+          },
+        },
+        fill: {
+          opacity: 1,
+        },
+        colors: [
+          function ({ value }) {
+            if (value < 40) {
+              return "#7E36AF";
+            } else {
+              return "#D9534F";
+            }
           },
         ],
-      },
-      options: {
-        responsive: true,
+        tooltip: {
+          y: {
+            formatter: function (val) {
+              return "$ " + val + " thousands";
+            },
+          },
+        },
       },
     });
 
-    return { bar, doughnut };
+    const doughnut = reactive({
+      type: "donut",
+      series: [44, 55, 41, 17, 15],
+      chartOptions: {
+        chart: {
+          type: "donut",
+        },
+        theme: {
+          mode: "light",
+          palette: "palette2",
+        },
+        responsive: [
+          {
+            breakpoint: 880,
+            options: {
+              chart: {
+                width: 320,
+              },
+              legend: {
+                position: "bottom",
+                fontSize: "12px",
+              },
+            },
+          },
+        ],
+        legend: {
+          fontSize: "16px",
+        },
+      },
+    });
+
+    return { bar, doughnut, clientWidth };
   },
 };
 </script>
@@ -67,5 +149,11 @@ export default {
 .wrapper {
   width: 60%;
   margin: 0 auto;
+  display: flex;
+  justify-content: center;
+
+  .vue-apexcharts {
+    width: max-content;
+  }
 }
 </style>
