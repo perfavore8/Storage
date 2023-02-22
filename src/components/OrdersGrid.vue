153cf7f655aae311f2c3df344f7868d7c321ff02
perfavore8 <nekit2353@mail.ru>
2023-02-22 17:16:32 +0200

    some refactoring

--- src/components/OrdersGrid.vue
+++ src/components/OrdersGrid.vue
@@ -36,7 +36,7 @@
         </thead>
         <tbody v-if="products.list.length">
           <template v-for="(row, idx) in products.list" :key="row">
-            <tr class="row" @click="row.isOpen = !row.isOpen">
+            <tr class="row">
               <td class="item">
                 <input
                   type="checkbox"
@@ -48,10 +48,38 @@
                 <label :for="row + idx"></label>
               </td>
               <template v-for="title in products.titiles" :key="title">
-                <td class="item" v-if="title.code === 'stat'">
+                <td
+                  class="item"
+                  v-if="title.code === 'names'"
+                  style="padding: 5px 10px 5px 15px"
+                >
                   <div class="stat">
                     <img :src="row.img" class="img" />
-                    <span>{{ row.stat }}</span>
+                    <a
+                      target="black"
+                      class="underline text-indigo-500 hover:text-indigo-600"
+                      :href="
+                        'https://' +
+                        accountSubdomain +
+                        '.amocrm.ru/leads/detail/' +
+                        row.names
+                      "
+                    >
+                      {{ row.names }}
+                    </a>
+                  </div>
+                </td>
+                <td class="item" v-else-if="title.code === 'poz'">
+                  <div class="flex flex-row gap-4 items-center">
+                    <span class="font-medium text-base">{{
+                      row.poz.list.length
+                    }}</span>
+                    <button
+                      class="btn btn_grey"
+                      @click="openModal(row.poz.list)"
+                    >
+                      Показать
+                    </button>
                   </div>
                 </td>
                 <td class="item" v-else>
@@ -59,19 +87,18 @@
                 </td>
               </template>
             </tr>
-            <tr
+            <!-- <tr
               class="row hidden-row"
               v-for="row2 in row.list"
               :key="row2"
               v-show="row.isOpen"
             >
               <td class="item"></td>
-              <td class="item"></td>
               <template v-for="title in products.titiles" :key="title">
                 <td class="item">{{ row2[title.code] }}</td>
               </template>
             </tr>
-            <tr v-if="row.isOpen" class="space" />
+            <tr v-if="row.isOpen" class="space" /> -->
           </template>
         </tbody>
       </table>
@@ -89,15 +116,27 @@
       @changePage="changePage"
     />
   </div>
+  <Teleport to="body">
+    <ReportGridModal
+      v-if="selectedOrder?.isOpen"
+      :title="selectedOrder?.title"
+      :list="selectedOrder?.list"
+      @close="closeOrderGridModal"
+    >
+      <template v-slot:title>Позиции</template>
+    </ReportGridModal>
+  </Teleport>
 </template>
 
 <script>
 import GridBottom from "@/components/GridBottom.vue";
+import ReportGridModal from "./ReportGridModal.vue";
 // import { nextTick } from "vue";
 export default {
   name: "Main_grid",
   components: {
     GridBottom,
+    ReportGridModal,
   },
 
   data() {
@@ -115,8 +154,42 @@ export default {
         "https://www.logobank.ru/images/ph/ru/0-9/1kanal.png",
         "https://www.logobank.ru/images/ph/en/a/apple.png",
       ],
+      whOptions: [],
+      priceCatOptions: [],
+      selectedOrder: {
+        isOpen: false,
+        title: [
+          { name: "Название", code: "name" },
+          { name: "Артикул", code: "article" },
+          { name: "Партия", code: "batch" },
+          { name: "Количество", code: "count" },
+          { name: "Склад", code: "wh" },
+          { name: "Цена", code: "price" },
+          { name: "Валюта", code: "price_currency" },
+          { name: "Тип цены", code: "price_type" },
+        ],
+        list: [],
+      },
+      statList: [
+        {
+          name: "Открытый",
+          value: 1,
+        },
+        {
+          name: "Закрытый",
+          value: 0,
+        },
+        // {
+        //   name: "Отмененный",
+        //   value: 3,
+        // },
+      ],
       products: {
         titiles: [
+          {
+            name: "Название сделки",
+            code: "names",
+          },
           {
             name: "Статус заказа",
             code: "stat",
@@ -136,46 +209,47 @@ export default {
           {
             name: "Список позиций",
             code: "poz",
+            type: 2,
           },
         ],
         list: [
-          {
-            isOpen: false,
-            otv: "Егор Кондратенко",
-            date: "22.12.2022",
-            sum: "123123 RUB",
-            stat: "Открытый",
-            poz: "Заготовка обсадная 127х9,199999999999999, гр.пр. М, треугольная удлиненная  ",
-            img: "https://www.logobank.ru/images/ph/ru/v/vtb_new_logo_2018.png",
-            list: [
-              {
-                otv: "Егор Кондратенко",
-                sum: "123123 RUB",
-                poz: "Заготовка обсадная 127х9,199999999999999, гр.пр. М, треугольная удлиненная  ",
-              },
-            ],
-          },
-          {
-            isOpen: false,
-            otv: "Ефим Ефимович, Александр Заболотный",
-            date: "22.12.2022",
-            sum: "3500 RUB",
-            stat: "Закрытый",
-            poz: "Треники, Стол",
-            img: "https://www.logobank.ru/images/ph/en/a/apple.png",
-            list: [
-              {
-                otv: "Ефим Ефимович",
-                sum: "500 RUB",
-                poz: "Треники",
-              },
-              {
-                otv: "Александр Заболотный",
-                sum: "3000 RUB",
-                poz: "Стол",
-              },
-            ],
-          },
+          // {
+          //   isOpen: false,
+          //   otv: "Егор Кондратенко",
+          //   date: "22.12.2022",
+          //   sum: "123123 RUB",
+          //   stat: "Открытый",
+          //   poz: "Заготовка обсадная 127х9,199999999999999, гр.пр. М, треугольная удлиненная  ",
+          //   img: "https://www.logobank.ru/images/ph/ru/v/vtb_new_logo_2018.png",
+          //   list: [
+          //     {
+          //       otv: "Егор Кондратенко",
+          //       sum: "123123 RUB",
+          //       poz: "Заготовка обсадная 127х9,199999999999999, гр.пр. М, треугольная удлиненная  ",
+          //     },
+          //   ],
+          // },
+          // {
+          //   isOpen: false,
+          //   otv: "Ефим Ефимович, Александр Заболотный",
+          //   date: "22.12.2022",
+          //   sum: "3500 RUB",
+          //   stat: "Закрытый",
+          //   poz: "Треники, Стол",
+          //   img: "https://www.logobank.ru/images/ph/en/a/apple.png",
+          //   list: [
+          //     {
+          //       otv: "Ефим Ефимович",
+          //       sum: "500 RUB",
+          //       poz: "Треники",
+          //     },
+          //     {
+          //       otv: "Александр Заболотный",
+          //       sum: "3000 RUB",
+          //       poz: "Стол",
+          //     },
+          //   ],
+          // },
         ],
       },
       orders: {},
@@ -183,10 +257,15 @@ export default {
     };
   },
   async mounted() {
-    this.setSelectedProducts();
+    await this.$store.dispatch("get_all_fields");
+    await this.$store.dispatch("get_account");
     await this.$store.dispatch("getOrders", this.$store.state.orders.filters);
-    this.orders = { ...this.$store.state.orders.orders };
+    this.fillWhs();
+    this.fillPriceCat();
+    this.orders = [...this.$store.state.orders.orders];
     this.filters = { ...this.$store.state.orders.filters };
+    this.fillOrders();
+    this.setSelectedProducts();
   },
 
   computed: {
@@ -198,6 +277,12 @@ export default {
     },
     meta() {
       return this.$store.state.orders.meta;
+    },
+    fields() {
+      return this.$store.state.fields.all_fields;
+    },
+    accountSubdomain() {
+      return this.$store.state.account.account.subdomain;
     },
   },
 
@@ -223,6 +308,69 @@ export default {
     getOrders() {
       this.$store.dispatch("getOrders", this.$store.state.orders.filters);
     },
+    fillWhs() {
+      this.fields
+        .filter((val) => val.type == 13 && val.code != "whs")
+        .forEach((val) =>
+          this.whOptions.push({ name: val.name, value: val.code })
+        );
+    },
+    fillPriceCat() {
+      this.fields
+        .filter((val) => val.type == 11)
+        .forEach((val) =>
+          this.priceCatOptions.push({ name: val.name, value: val.code })
+        );
+    },
+    openModal(list) {
+      this.selectedOrder.list = list;
+      this.selectedOrder.isOpen = true;
+    },
+    closeOrderGridModal() {
+      this.selectedOrder.isOpen = false;
+    },
+    fillOrders() {
+      this.orders.forEach((order) => {
+        const poz = [];
+        order.positions.forEach((pos) => {
+          const a = {
+            name: pos?.name,
+            article: pos?.article,
+            batch: pos?.batch,
+            wh_field: pos?.wh_field,
+            count: pos?.count,
+            price_field: pos?.price_field,
+            price: pos?.price,
+            price_currency: pos?.price_currency,
+            wh: this.whOptions.find((el) => el.value == pos.wh_field)?.name,
+            price_type: this.priceCatOptions.find(
+              (el) => el.value == pos.price_field
+            )?.name,
+          };
+          poz.push(a);
+        });
+        const obj = {
+          // isOpen: false,
+          names: order.lead_id,
+          otv: order.responsible_name,
+          date: order.created_at,
+          sum: order.sum + " " + order.price_currency,
+          stat: this.statList.find((el) => el.value == order.is_active).name,
+          poz: {
+            list: [...poz],
+          },
+          img: "https://www.digiseller.ru/preview/571523/p1_3380359_3410fdc6.png",
+          // list: [
+          //   {
+          //     otv: "Егор Кондратенко",
+          //     sum: "123123 RUB",
+          //     poz: "Заготовка обсадная 127х9,199999999999999, гр.пр. М, треугольная удлиненная  ",
+          //   },
+          // ],
+        };
+        this.products.list.push(obj);
+      });
+    },
   },
 };
 </script>
@@ -231,13 +379,14 @@ export default {
 @import "@/app.scss";
 .img {
   object-fit: cover;
-  width: 24px;
+  width: 46px;
   padding: 4px;
   border: 1px solid #aeaeae;
   border-radius: 50%;
   object-fit: scale-down;
 }
 .stat {
+  width: max-content;
   display: flex;
   flex-direction: row;
   align-items: center;
@@ -262,7 +411,7 @@ export default {
     color: #000000;
     vertical-align: middle;
     text-align: center;
-    cursor: pointer;
+    // cursor: pointer;
     label {
       cursor: inherit;
     }
@@ -282,7 +431,7 @@ export default {
   }
 }
 .row {
-  cursor: pointer;
+  // cursor: pointer;
 }
 .space {
   height: 16px;
