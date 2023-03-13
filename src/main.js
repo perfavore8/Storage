f53a1de82be0fd427fa3a8e32cf34fb665bceb41
perfavore8 <nekit2353@mail.ru>
2023-03-13 18:50:18 +0200

    add vue-the-mask library

--- src/main.js
+++ src/main.js
@@ -3,6 +3,12 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import VueApexCharts from "vue3-apexcharts";
+import VueTheMask from "vue-the-mask";
 import "@/tailwind.css";
 import "material-icons/iconfont/material-icons.css";
-createApp(App).use(store).use(router).use(VueApexCharts).mount("#app");
+createApp(App)
+  .use(store)
+  .use(router)
+  .use(VueApexCharts)
+  .use(VueTheMask)
+  .mount("#app");
