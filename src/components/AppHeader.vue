c96fc7cd2f4353b00d5691f1e32bdb81fa29113e
perfavore8 <nekit2353@mail.ru>
2023-03-16 18:09:56 +0200

    add

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,27 @@
+<template>
+  <header>
+    <NavBar />
+    <HomeMenu />
+  </header>
+  <HomeModals />
+</template>
+
+<script>
+import NavBar from "@/components/NavBar.vue";
+import HomeMenu from "@/components/HomeMenu.vue";
+import HomeModals from "@/components/HomeModals.vue";
+export default {
+  components: { NavBar, HomeMenu, HomeModals },
+  setup() {},
+};
+</script>
+
+<style lang="scss" scoped>
+header {
+  width: 100%;
+  max-width: calc(100vw - 20px);
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+</style>
