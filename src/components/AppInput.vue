1d44fc0779dc51d2eba8f0ee56d4663f2ff5606f
perfavore8 <nekit2353@mail.ru>
2023-03-20 19:28:22 +0200

    add appinput

---
+++ src/components/AppInput.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="text">
+    <input
+      type="text"
+      class="input"
+      :placeholder="placeholder"
+      :value="inputValue"
+      @input="$emit('update:inputValue', $event.target.value)"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    inputValue: {
+      type: String,
+      required: false,
+    },
+    placeholder: {
+      type: String,
+      required: false,
+    },
+  },
+  setup() {},
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@/app.scss";
+.text {
+  margin: 0 3px;
+}
+</style>
