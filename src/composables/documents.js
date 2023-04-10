7168b0d46c81a3ccc95f4a40ae29bc1d8278d4d8
perfavore8 <nekit2353@mail.ru>
2023-04-10 13:38:32 +0200

    add status_id

--- src/composables/documents.js
+++ src/composables/documents.js
@@ -4,6 +4,22 @@ import { computed, reactive } from "vue";
 const documents = reactive({
   titles: [
     {
+      name: "Статус",
+      placeholder: "Статус документа",
+      code: "status_id",
+      isDocumentLink: false,
+      type: "AppMultiSelect",
+      minLength: 0,
+      requestDelay: 0,
+      value: "",
+      selected: {},
+      width: 1,
+      sortable: true,
+      isAutocomplete: false,
+      url: "document/templates",
+      list: [],
+    },
+    {
       name: "Название документа",
       placeholder: "Название документа",
       code: "template_name",
@@ -103,6 +119,12 @@ export function useDocuments() {
   const fillDocuments = () => {
     documents.list = [];
     documentsRaw.value.forEach((document) => {
+      const item = documents.titles.find((el) => el.code === "status_id");
+      const search = (id) => {
+        let res = item?.list?.find((val) => val.id == document?.[id])?.name;
+        if (!res) res = "";
+        return res;
+      };
       const obj = {
         template_name: document?.name,
         document_link: document?.url,
@@ -111,7 +133,10 @@ export function useDocuments() {
         contact_name: document?.contact_name,
         user_name: document?.user_name,
         created_at: document?.created_at,
+        status_id:
+          search(document?.pipeline_id) + "-" + search(document?.status_id),
       };
+      if (obj.status_id === "-") obj.status_id = "";
       documents.list.push(obj);
     });
   };
@@ -126,6 +151,25 @@ export function useDocuments() {
         title.list = [];
         res.forEach((el) => title.list.push({ name: el, value: el }));
       }
+    });
+  };
+
+  const fillStatuses = async () => {
+    const item = documents.titles.find((val) => val.code === "status_id");
+    item.list.push({ name: "Все", value: "all", id: -1 });
+    await store.dispatch("getPipelinesListV2");
+    const pipelinesListV2 = store.state.account.pipelinesListV2;
+    Object.entries(pipelinesListV2).forEach(([key, value]) => {
+      item.list.push({ name: value.name, value: "optgroup", id: key });
+      value.statuses.forEach((stat) => {
+        item.list.push({
+          name: stat.name,
+          value: key + "-" + stat.id,
+          id: stat.id,
+          color: stat.color,
+          optgroup: true,
+        });
+      });
     });
   };
 
@@ -145,6 +189,7 @@ export function useDocuments() {
   };
 
   const getDocuments = async () => {
+    fillStatuses();
     await store.dispatch("getDocuments", store.state.documents.filters);
     fillDocuments();
   };
