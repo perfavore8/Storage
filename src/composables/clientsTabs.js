7e739da62c0474acd5362470e087c03fd32ad326
perfavore8 <nekit2353@mail.ru>
2023-04-28 10:05:45 +0200

    global value

--- src/composables/clientsTabs.js
+++ src/composables/clientsTabs.js
@@ -1,24 +1,23 @@
 import { reactive } from "vue";
 
+const tabs = reactive({
+  selected: {
+    name: "Компании",
+    value: "Company",
+    component: "EditCompanySection",
+  },
+  list: [
+    {
+      name: "Контакты",
+      value: "Contacts",
+      component: "EditContactsSection",
+    },
+    { name: "Компании", value: "Company", component: "EditCompanySection" },
+  ],
+  select: function (option) {
+    this.selected = option;
+  },
+});
 export function useClientsTabs() {
-  const tabs = reactive({
-    selected: {
-      name: "Компании",
-      value: "Company",
-      component: "EditCompanySection",
-    },
-    list: [
-      {
-        name: "Контакты",
-        value: "Contacts",
-        component: "EditContactsSection",
-      },
-      { name: "Компании", value: "Company", component: "EditCompanySection" },
-    ],
-    select: function (option) {
-      this.selected = option;
-    },
-  });
-
   return { tabs };
 }
