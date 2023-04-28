3c9b3ac5bf10fab49286306e7af88f2bdaf57318
perfavore8 <nekit2353@mail.ru>
2023-04-28 12:54:42 +0200

    more values

--- src/composables/clientsTabs.js
+++ src/composables/clientsTabs.js
@@ -2,17 +2,33 @@ import { reactive } from "vue";
 
 const tabs = reactive({
   selected: {
-    name: "Компании",
-    value: "Company",
-    component: "EditCompanySection",
+    name: "Контакты",
+    value: "Contacts",
+    value2: "contact",
+    value3: "contacts",
+    oppositeValue: "Company",
+    oppositeValue2: "company",
+    oppositeValue3: "companies",
   },
   list: [
     {
       name: "Контакты",
       value: "Contacts",
-      component: "EditContactsSection",
+      value2: "contact",
+      value3: "contacts",
+      oppositeValue: "Company",
+      oppositeValue2: "company",
+      oppositeValue3: "companies",
     },
-    { name: "Компании", value: "Company", component: "EditCompanySection" },
+    {
+      name: "Компании",
+      value: "Company",
+      value2: "company",
+      value3: "companies",
+      oppositeValue: "Contacts",
+      oppositeValue2: "contact",
+      oppositeValue3: "contacts",
+    },
   ],
   select: function (option) {
     this.selected = option;
