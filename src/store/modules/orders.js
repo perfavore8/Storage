1653e17a759bd193feb4c577cab5df193d122ddb
perfavore8 <nekit2353@mail.ru>
2023-05-11 11:11:26 +0200

    add actions

--- src/store/modules/orders.js
+++ src/store/modules/orders.js
@@ -3,6 +3,8 @@ export default {
   state: {
     orders: {},
     meta: {},
+    types: [],
+    fields: [],
     filters: {
       page: 1,
     },
@@ -23,6 +25,24 @@ export default {
       delete value.data;
       state.meta = { ...value };
     },
+    updateOrdersTypes(state, value) {
+      state.types = [...Object.values(value)];
+    },
+    updateOrdersFields(state, value) {
+      const componentsList = {
+        1: "EditInteger",
+        2: "EditFloat",
+        3: "EditString",
+        4: "EditText",
+        5: "EditSelector",
+        6: "EditMultiSelector",
+        7: "EditDate",
+        8: "EditDateTime",
+        9: "EditFlag",
+      };
+      value.map((field) => (field.component = componentsList[field.type]));
+      state.fields = [...value];
+    },
   },
   actions: {
     async getOrders(context, params) {
@@ -40,6 +60,59 @@ export default {
       context.commit("updateOrders", json.data);
       context.commit("updateMeta", json);
       context.commit("updateIsLoading", false);
+    },
+
+    async getOrdersTypes(context) {
+      const url = BaseURL + "orders/field/types";
+      const res = await fetch(url, {
+        headers: { Authorization: TOKEN },
+      });
+      const json = await res.json();
+      context.commit("updateOrdersTypes", json);
+    },
+
+    async getOrdersFields(context) {
+      const url = BaseURL + "orders/field/list";
+      const res = await fetch(url, {
+        headers: { Authorization: TOKEN },
+      });
+      const json = await res.json();
+      context.commit("updateOrdersFields", json);
+    },
+    async addOrdersField(context, params) {
+      const url = BaseURL + "orders/field/add";
+      const res = await fetch(url, {
+        method: "POST",
+        headers: {
+          Authorization: TOKEN,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(params),
+      });
+      const json = await res.json();
+      return json;
+    },
+    async updateOrdersField(context, params) {
+      const url = BaseURL + "orders/field/update";
+      await fetch(url, {
+        method: "POST",
+        headers: {
+          Authorization: TOKEN,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(params),
+      });
+    },
+    async deleteOrdersField(context, params) {
+      const url = BaseURL + "orders/field/delete";
+      await fetch(url, {
+        method: "POST",
+        headers: {
+          Authorization: TOKEN,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(params),
+      });
     },
   },
 };
