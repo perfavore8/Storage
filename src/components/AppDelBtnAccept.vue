f1d27a1c280fb4a548499c00694a54f36017e8e7
perfavore8 <nekit2353@mail.ru>
2023-05-16 10:58:02 +0200

    new del btn

---
+++ src/components/AppDelBtnAccept.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="relative flex justify-center">
+    <button
+      ref="btn"
+      class="btn text-white bg-red-600"
+      tabindex="1"
+      @click="toggleAccept()"
+    >
+      Удалить
+    </button>
+    <transition name="fade">
+      <div
+        v-if="showAccept"
+        class="absolute bottom-full mb-2 z-10 px-4 py-2 flex flex-col items-center gap-2 origin-bottom transform scale-100 rounded-md bg-white opacity-100 shadow-lg ring-1 ring-gray-900 ring-opacity-5 transition-all in"
+        ref="target"
+      >
+        <span class="whitespace-nowrap text-gray-900">Точно удалить?</span>
+        <div class="flex flex-row gap-2">
+          <button
+            class="btn small_btn btn_light_dark_blue"
+            @click="confirm()"
+            tabindex="2"
+          >
+            Да
+          </button>
+          <button class="btn small_btn btn_grey" @click="reset()" tabindex="2">
+            Нет
+          </button>
+        </div>
+      </div>
+    </transition>
+  </div>
+</template>
+
+<script>
+import { onClickOutside, useToggle } from "@vueuse/core";
+import { ref } from "vue";
+export default {
+  setup(props, context) {
+    const target = ref(null);
+    const btn = ref(null);
+    onClickOutside(target, () => reset(), { ignore: [btn] });
+
+    const [showAccept, toggleAccept] = useToggle(false);
+
+    const confirm = () => context.emit("confirm");
+    const reset = () => toggleAccept(false);
+
+    return { showAccept, toggleAccept, target, confirm, reset, btn };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@/app.scss";
+.fade-enter-active,
+.fade-leave-active {
+  transition: all 0.1s ease-out;
+}
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+  transform: scale(0.5);
+}
+.btn {
+  line-height: 1.2;
+}
+</style>
