f34d3afa8f3e239aef1e4f78dc789314856e0345
perfavore8 <nekit2353@mail.ru>
2023-06-21 10:37:13 +0200

    fix

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -216,10 +216,14 @@ export default {
       let arr = [];
       this.isGrid
         ? (arr = [
-            ...this.ref_card?.selectedProducts.filter((val) => val.value),
+            ...this.ref_card?.selectedProducts.filter(
+              (val) => val.value && val.item
+            ),
           ])
         : (arr = [
-            ...this.ref_main?.selectedProducts.filter((val) => val.value),
+            ...this.ref_main?.selectedProducts.filter(
+              (val) => val.value && val.item
+            ),
           ]);
       return arr;
     },
