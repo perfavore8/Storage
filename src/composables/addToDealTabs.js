351683262298b2628e54dc741405cbf2023dce11
perfavore8 <nekit2353@mail.ru>
2023-07-25 15:53:55 +0300

    unes

--- src/composables/addToDealTabs.js
+++ src/composables/addToDealTabs.js
@@ -1,6 +1,5 @@
 import { computed } from "vue";
 import { reactive } from "vue";
-console.log(123);
 
 const tabs = reactive({
   selected: { name: "Товары", value: "products", code: "ProductsTab" },
