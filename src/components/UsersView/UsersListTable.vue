baea4d43ea995c8b7c99140e85b88dd54862bc02
perfavore8 <nekit2353@mail.ru>
2023-08-08 13:47:05 +0200

    chore done usersVeiw

---
+++ src/components/UsersView/UsersListTable.vue
@@ -0,0 +1,260 @@
+<template>
+  <div
+    class="wrapper divide-y divide-slate-200/70 rounded-xl border border-solid border-slate-200/70 shadow-lg shadow-gray-400/10"
+  >
+    <div
+      class="filters px-5 py-4 flex flex-row gap-2 justify-between items-center"
+    >
+      <div class="left">
+        <div class="search w-full relative">
+          <AppInput
+            v-model:inputValue="searchValue"
+            :placeholder="'Поиск пользователей'"
+            :underlined="true"
+          />
+          <span
+            class="material-icons-round absolute right-3 -translate-y-1/2 top-1/2 text-shark-500 cursor-pointer"
+          >
+            search
+          </span>
+        </div>
+      </div>
+      <div class="right">
+        <button
+          class="export btn order-1 max-h-[34px] pointer-events-auto relative inline-flex items-center gap-2 whitespace-nowrap w-fit rounded-md bg-white text-[0.8125rem] font-medium leading-5 text-slate-700 shadow-sm ring-1 ring-slate-700/10 hover:bg-slate-50 hover:text-slate-900 hover:disabled:bg-white disabled:opacity-30 disabled:cursor-not-allowed"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="18"
+            height="18"
+            viewBox="0 0 24 24"
+          >
+            <path
+              fill="currentColor"
+              d="M8.71 7.71L11 5.41V15a1 1 0 0 0 2 0V5.41l2.29 2.3a1 1 0 0 0 1.42 0a1 1 0 0 0 0-1.42l-4-4a1 1 0 0 0-.33-.21a1 1 0 0 0-.76 0a1 1 0 0 0-.33.21l-4 4a1 1 0 1 0 1.42 1.42ZM21 14a1 1 0 0 0-1 1v4a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1v-4a1 1 0 0 0-2 0v4a3 3 0 0 0 3 3h14a3 3 0 0 0 3-3v-4a1 1 0 0 0-1-1Z"
+            />
+          </svg>
+          Экспорт
+        </button>
+      </div>
+    </div>
+    <div class="main">
+      <table class="w-full text-slate-500">
+        <thead class="bg-slate-50 border-y border-solid border-slate-200/70">
+          <tr>
+            <th>
+              <input type="checkbox" class="checkbox" id="rowid" />
+              <label for="rowid"></label>
+            </th>
+            <th v-for="title in titiles" :key="title.code">{{ title.name }}</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="user in users" :key="user.id">
+            <td>
+              <input type="checkbox" class="checkbox" :id="user.id" />
+              <label :for="user.id"></label>
+            </td>
+            <td v-for="title in titiles" :key="title.code">
+              <template v-if="title.code === 'name'">
+                <div class="w-fit h-full flex flex-row gap-4 items-center">
+                  <!-- <img
+                    src="https://rozemarin.ru/local/templates/new_template/img/icon/avatar@3x.png"
+                    class="h-10 aspect-auto"
+                  /> -->
+                  <div
+                    class="h-10 w-10 rounded-full flex justify-center items-center font-semibold text-xl"
+                    :style="{
+                      backgroundColor: user.color + '20',
+                      color: user.color,
+                    }"
+                  >
+                    {{ firstLetter(user.name) }}
+                  </div>
+                  <div class="flex flex-col">
+                    <h3 class="text-gray-900 font-semibold">{{ user.name }}</h3>
+                    <span>{{ user.email }}</span>
+                  </div>
+                </div>
+              </template>
+              <template v-else-if="title.code === 'status'">
+                <div class="w-fit h-full flex flex-row gap-4 items-center">
+                  <div
+                    class="w-4 h-4 rounded-full flex items-center justify-center"
+                    :style="{
+                      'background-color':
+                        (user.status === 2
+                          ? '#4ade80'
+                          : user.status === 1
+                          ? '#6b7280'
+                          : '#fb7185') + '50',
+                    }"
+                  >
+                    <div
+                      class="w-2 h-2 rounded-full"
+                      :style="{
+                        'background-color':
+                          (user.status === 2
+                            ? '#4ade80'
+                            : user.status === 1
+                            ? '#6b7280'
+                            : '#fb7185') + '99',
+                      }"
+                    ></div>
+                  </div>
+                  {{
+                    user.status === 2
+                      ? "В сети"
+                      : user.status === 1
+                      ? "Отошел"
+                      : "Не в сети"
+                  }}
+                </div>
+              </template>
+              <template v-else>{{ user[title.code] }}</template>
+            </td>
+            <td>
+              <button
+                class="export btn order-1 max-h-[34px] pointer-events-auto relative inline-flex items-center gap-2 whitespace-nowrap w-fit rounded-md bg-white text-[0.8125rem] font-medium leading-5 shadow-sm ring-1 ring-slate-700/10 hover:bg-slate-50 hover:text-slate-900 hover:disabled:bg-white disabled:opacity-30 disabled:cursor-not-allowed"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="20"
+                  height="20"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    fill="currentColor"
+                    d="m19.3 8.925l-4.25-4.2l1.4-1.4q.575-.575 1.413-.575t1.412.575l1.4 1.4q.575.575.6 1.388t-.55 1.387L19.3 8.925ZM17.85 10.4L7.25 21H3v-4.25l10.6-10.6l4.25 4.25Z"
+                  />
+                </svg>
+                Редактировать
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <div class="pagination p-5 flex justify-between items-center">
+      <div class="left flex flex-row gap-1 items-center text-slate-500">
+        Показывать:
+        <GridBottom
+          :count="20"
+          :showBtns="false"
+          @changeCount="null"
+          class="!my-0 mr-2"
+        />
+        из {{ page.total }}
+      </div>
+      <div class="right">
+        <AppPaginator :page="page" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { computed, reactive } from "vue";
+import AppInput from "../AppInput.vue";
+import GridBottom from "../GridBottom.vue";
+import AppPaginator from "../AppPaginator.vue";
+import { useColor } from "@/composables/color";
+export default {
+  components: { AppInput, GridBottom, AppPaginator },
+  setup() {
+    const { getRandomColor2 } = useColor();
+
+    const page = reactive({
+      total: 50,
+      first: 1,
+      prev: null,
+      current: 1,
+      next: 2,
+      last: 3,
+    });
+
+    const titiles = reactive([
+      { name: "Имя", code: "name" },
+      { name: "Группа", code: "group" },
+      { name: "Роль", code: "role" },
+      { name: "Статус", code: "status" },
+    ]);
+    const users = reactive([
+      {
+        id: 0,
+        name: "Ella Lauda",
+        email: "amanda@site.com",
+        group: "Москва",
+        role: "Пользователь",
+        status: 2,
+        color: computed(() => getRandomColor2().toLocaleLowerCase()),
+      },
+      {
+        id: 1,
+        name: "John Doe",
+        email: "john.doe@example.com",
+        group: "Москва",
+        role: "Администратор",
+        status: 2,
+        color: computed(() => getRandomColor2().toLocaleLowerCase()),
+      },
+      {
+        id: 2,
+        name: "Jane Smith",
+        email: "jane.smith@example.com",
+        group: "СПБ",
+        role: "Пользователь",
+        status: 0,
+        color: computed(() => getRandomColor2().toLocaleLowerCase()),
+      },
+      {
+        id: 3,
+        name: "Michael Johnson",
+        email: "michael.johnson@example.com",
+        group: "СПБ",
+        role: "Пользователь",
+        status: 1,
+        color: computed(() => getRandomColor2().toLocaleLowerCase()),
+      },
+      {
+        id: 4,
+        name: "Emily Wilson",
+        email: "emily.wilson@example.com",
+        group: "СПБ",
+        role: "Пользователь",
+        status: 0,
+        color: computed(() => getRandomColor2().toLocaleLowerCase()),
+      },
+    ]);
+
+    const firstLetter = (str) => str[0].toLocaleUpperCase();
+
+    return { page, titiles, users, getRandomColor2, firstLetter };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@/app.scss";
+
+.left {
+  width: clamp(100px, 30%, 300px);
+}
+th,
+td {
+  max-width: 200px;
+  @apply py-4 px-5 text-left;
+}
+
+th:first-child,
+td:first-child {
+  width: 1%;
+}
+
+th:last-child,
+td:last-child {
+  width: 1%;
+  min-width: 211px;
+}
+</style>
--- src/views/UsersView.vue
+++ src/views/UsersView.vue
@@ -1,9 +1,109 @@
-<template>123</template>
+<template>
+  <div class="wrapper p-6 w-screen">
+    <div
+      class="header flex flex-row justify-between items-center pb-8 mb-8 border-b border-solid border-slate-200/70"
+    >
+      <div class="left flex flex-col items-start">
+        <AppBreadcrumb :pages="pages" />
+        <h1 class="text-2xl text-gray-900 font-semibold">
+          {{ forTitle?.text }}
+        </h1>
+      </div>
+      <div class="right">
+        <button class="btn btn_blue flex gap-2 !py-2 !h-10">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+          >
+            <path
+              fill="currentColor"
+              d="M21 14h-6a1 1 0 1 1 0-2h6a1 1 0 1 1 0 2zm-3 3a1 1 0 0 1-1-1v-6a1 1 0 1 1 2 0v6a1 1 0 0 1-1 1zM9 6c1.654 0 3 1.346 3 3s-1.346 3-3 3s-3-1.346-3-3s1.346-3 3-3m0-2C6.236 4 4 6.238 4 9s2.236 5 5 5s5-2.238 5-5s-2.236-5-5-5zm0 13c2.021 0 3.301.771 3.783 1.445C12.1 18.705 10.814 19 9 19c-1.984 0-3.206-.305-3.818-.542C5.641 17.743 6.959 17 9 17m0-2c-3.75 0-6 2-6 4c0 1 2.25 2 6 2c3.518 0 6-1 6-2c0-2-2.354-4-6-4z"
+            />
+          </svg>
+          Добавить
+        </button>
+      </div>
+    </div>
+    <div class="main flex flex-col gap-8">
+      <UsersActivity :list="usersActivity" />
+      <UsersListTable />
+    </div>
+  </div>
+</template>
 
 <script>
+import AppBreadcrumb from "@/components/AppBreadcrumb.vue";
+import { computed, reactive } from "vue";
+import { useRouter } from "vue-router";
+import UsersActivity from "@/components/UsersView/UsersActivity.vue";
+import store from "@/store";
+import UsersListTable from "@/components/UsersView/UsersListTable.vue";
 export default {
-  setup() {},
+  components: { AppBreadcrumb, UsersActivity, UsersListTable },
+  setup() {
+    const router = useRouter();
+
+    const pages = reactive([
+      {
+        id: 1,
+        text: "<span class='material-icons-round' style='font-size: 20px'>home</span>",
+        select: () => router.push("/"),
+      },
+      {
+        id: 2,
+        forTitle: true,
+        text: "Пользователи",
+        select: () => {
+          router.push("/");
+          store.commit("open_close_settings");
+        },
+      },
+      {
+        id: 3,
+        text: "Список",
+        select: () => {},
+      },
+    ]);
+    const forTitle = computed(() => pages.find((el) => el.forTitle));
+
+    const usersActivity = reactive([
+      {
+        id: 1,
+        header: "TOTAL USERS",
+        firstNumber: "24",
+        secondNumber: "22",
+        stat: { text: "5.0%", direction: 1 },
+      },
+      {
+        id: 2,
+        header: "ACTIVE MEMBERS",
+        firstNumber: "12",
+        secondNumber: "11",
+        stat: { text: "1.2%", direction: 1 },
+      },
+      {
+        id: 3,
+        header: "NEW/RETURNING",
+        firstNumber: "56 %",
+        secondNumber: "48.7",
+        stat: { text: "2.8%", direction: 0 },
+      },
+      {
+        id: 4,
+        header: "ACTIVE MEMBERS",
+        firstNumber: "28 %",
+        secondNumber: "28.6%",
+        stat: { text: "0.0%", direction: 2 },
+      },
+    ]);
+
+    return { pages, forTitle, usersActivity };
+  },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@import "@/app.scss";
+</style>
