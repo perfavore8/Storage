f8864cd996d44008985688884b56a48fd5eab86f
perfavore8 <nekit2353@mail.ru>
2023-08-09 14:33:49 +0200

    fix another source

--- src/composables/availableUserAccounts.js
+++ src/composables/availableUserAccounts.js
@@ -13,13 +13,17 @@ export function useAvailableUserAccounts() {
       if (success) router.push(router.currentRoute.value.path);
     },
     setList: async function () {
-      const accounts = await store.dispatch("getUsersList");
-      accounts.map((account) => (account.value = account.id));
+      const accounts = store.state.account.user?.accounts;
+      accounts.map(
+        (account) => (
+          (account.value = account.id), (account.name = account.subdomain)
+        )
+      );
       this.list = accounts;
       this.setSelected();
     },
     setSelected: function () {
-      const id = store.state.account.user?.id;
+      const id = store.state.account.account?.id;
       this.selected = this.list.find((account) => account.id === id);
     },
   });
