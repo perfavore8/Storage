3a37d2c86bea975d163aad0d63cafb53f6526517
perfavore8 <nekit2353@mail.ru>
2023-08-14 15:14:59 +0200

    add vue-mask-email

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import router from "./router";
 import store from "./store";
 import VueApexCharts from "vue3-apexcharts";
 import VueTheMask from "vue-the-mask";
+import MaskEmail from "@yasanchezz/vue-mask-email";
+import "@yasanchezz/vue-mask-email/dist/style.css";
 import "@/tailwind.css";
 import "material-icons/iconfont/material-icons.css";
 createApp(App)
@@ -11,4 +13,5 @@ createApp(App)
   .use(router)
   .use(VueApexCharts)
   .use(VueTheMask)
+  .use(MaskEmail)
   .mount("#app");
