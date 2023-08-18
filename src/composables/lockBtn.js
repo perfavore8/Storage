2fb0c5765a221826cf5b7cacb0878a2a7e310aa1
perfavore8 <nekit2353@mail.ru>
2023-08-18 12:30:28 +0200

    feat add useLock

---
+++ src/composables/lockBtn.js
@@ -0,0 +1,29 @@
+import { useToggle } from "@vueuse/core";
+
+export function useLockBtn(
+  globalType = "timer",
+  globalDelay = 3000,
+  initialValue = false
+) {
+  // const types = [{ type: "timer" }, { type: "async" }, {type: "asyncWithTimer" }];
+
+  const [isLocked, toggleLock] = useToggle(initialValue);
+
+  const lockBtn = async (type = globalType, delay, promise) => {
+    toggleLock(true);
+    if (type === "timer")
+      setTimeout(() => toggleLock(false), delay || globalDelay);
+    if (type === "async" && promise) {
+      await promise;
+      toggleLock(false);
+    }
+    if (type === "asyncWithTimer" && promise) {
+      setTimeout(async () => {
+        await promise;
+        toggleLock(false);
+      }, delay || globalDelay);
+    }
+  };
+
+  return { isLocked, lockBtn };
+}
