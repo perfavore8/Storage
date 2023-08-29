ed12e9c781edbf55dd5540e4a4f580c38bd8e76b
perfavore8 <nekit2353@mail.ru>
2023-08-29 12:24:50 +0200

    feat add getRandomColor3

--- src/composables/color.js
+++ src/composables/color.js
@@ -1,4 +1,17 @@
 export function useColor() {
+  function hslToHex(h, s, l) {
+    l /= 100;
+    const a = (s * Math.min(l, 1 - l)) / 100;
+    const f = (n) => {
+      const k = (n + h / 30) % 12;
+      const color = l - a * Math.max(Math.min(k - 3, 9 - k, 1), -1);
+      return Math.round(255 * color)
+        .toString(16)
+        .padStart(2, "0"); // convert to Hex and prefix "0" if needed
+    };
+    return `#${f(0)}${f(8)}${f(4)}`;
+  }
+
   function getRandomColor() {
     var letters = "0123456789ABCDEF";
     var color = "#";
@@ -15,6 +28,14 @@ export function useColor() {
     }
     return color;
   }
+  function getRandomColor3(opacity) {
+    const h = Math.floor(Math.random() * 256);
 
-  return { getRandomColor, getRandomColor2 };
+    let color = hslToHex(h, 100, 50);
+    if (opacity) color = color + opacity;
+
+    return color;
+  }
+
+  return { getRandomColor, getRandomColor2, getRandomColor3 };
 }
