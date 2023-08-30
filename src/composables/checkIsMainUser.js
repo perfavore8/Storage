d4d2c1f0ade2c07e304e98da8ba7f3c1f7a014ab
perfavore8 <nekit2353@mail.ru>
2023-08-30 14:47:14 +0200

    fix

--- src/composables/checkIsMainUser.js
+++ src/composables/checkIsMainUser.js
@@ -1,7 +1,7 @@
 import store from "@/store";
 import { computed } from "vue";
 
-const { users } = store.dispatch("getUsersList");
+const users = store.dispatch("getUsersList");
 
 export function useCheckIsMainUser() {
   const isMain = computed(() => users.find((user) => user.is_main));
