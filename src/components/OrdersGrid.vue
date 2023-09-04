39a506fc6977ec7d653fd68acfddc8e264ffe226
perfavore8 <nekit2353@mail.ru>
2023-09-04 14:06:43 +0200

    chore done table setting

--- src/components/OrdersGrid.vue
+++ src/components/OrdersGrid.vue
@@ -1,23 +1,23 @@
 <template>
   <div class="main mt-20 relative">
-    <div
-      v-if="isTest || isMain"
-      @click="openTableSettings()"
-      class="tableSettings absolute top-2 right-2"
-    >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="20"
-        height="20"
-        viewBox="0 0 512 512"
+    <table class="table relative" ref="table" :class="{ blur: isDataLoading }">
+      <div
+        v-if="isTest"
+        @click="openTableSettings()"
+        class="tableSettings absolute top-1 right-1"
       >
-        <path
-          fill="#757575"
-          d="M495.9 166.6c3.2 8.7.5 18.4-6.4 24.6l-43.3 39.4c1.1 8.3 1.7 16.8 1.7 25.4s-.6 17.1-1.7 25.4l43.3 39.4c6.9 6.2 9.6 15.9 6.4 24.6c-4.4 11.9-9.7 23.3-15.8 34.3l-4.7 8.1c-6.6 11-14 21.4-22.1 31.2c-5.9 7.2-15.7 9.6-24.5 6.8l-55.7-17.7c-13.4 10.3-28.2 18.9-44 25.4l-12.5 57.1c-2 9.1-9 16.3-18.2 17.8c-13.8 2.3-28 3.5-42.5 3.5s-28.7-1.2-42.5-3.5c-9.2-1.5-16.2-8.7-18.2-17.8l-12.5-57.1c-15.8-6.5-30.6-15.1-44-25.4l-55.6 17.8c-8.8 2.8-18.6.3-24.5-6.8c-8.1-9.8-15.5-20.2-22.1-31.2l-4.7-8.1c-6.1-11-11.4-22.4-15.8-34.3c-3.2-8.7-.5-18.4 6.4-24.6l43.3-39.4c-1.1-8.4-1.7-16.9-1.7-25.5s.6-17.1 1.7-25.4l-43.3-39.4c-6.9-6.2-9.6-15.9-6.4-24.6c4.4-11.9 9.7-23.3 15.8-34.3l4.7-8.1c6.6-11 14-21.4 22.1-31.2c5.9-7.2 15.7-9.6 24.5-6.8l55.7 17.7c13.4-10.3 28.2-18.9 44-25.4l12.5-57.1c2-9.1 9-16.3 18.2-17.8C227.3 1.2 241.5 0 256 0s28.7 1.2 42.5 3.5c9.2 1.5 16.2 8.7 18.2 17.8l12.5 57.1c15.8 6.5 30.6 15.1 44 25.4l55.7-17.7c8.8-2.8 18.6-.3 24.5 6.8c8.1 9.8 15.5 20.2 22.1 31.2l4.7 8.1c6.1 11 11.4 22.4 15.8 34.3zM256 336a80 80 0 1 0 0-160a80 80 0 1 0 0 160z"
-        />
-      </svg>
-    </div>
-    <table class="table" ref="table" :class="{ blur: isDataLoading }">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="20"
+          height="20"
+          viewBox="0 0 512 512"
+        >
+          <path
+            fill="#757575"
+            d="M495.9 166.6c3.2 8.7.5 18.4-6.4 24.6l-43.3 39.4c1.1 8.3 1.7 16.8 1.7 25.4s-.6 17.1-1.7 25.4l43.3 39.4c6.9 6.2 9.6 15.9 6.4 24.6c-4.4 11.9-9.7 23.3-15.8 34.3l-4.7 8.1c-6.6 11-14 21.4-22.1 31.2c-5.9 7.2-15.7 9.6-24.5 6.8l-55.7-17.7c-13.4 10.3-28.2 18.9-44 25.4l-12.5 57.1c-2 9.1-9 16.3-18.2 17.8c-13.8 2.3-28 3.5-42.5 3.5s-28.7-1.2-42.5-3.5c-9.2-1.5-16.2-8.7-18.2-17.8l-12.5-57.1c-15.8-6.5-30.6-15.1-44-25.4l-55.6 17.8c-8.8 2.8-18.6.3-24.5-6.8c-8.1-9.8-15.5-20.2-22.1-31.2l-4.7-8.1c-6.1-11-11.4-22.4-15.8-34.3c-3.2-8.7-.5-18.4 6.4-24.6l43.3-39.4c-1.1-8.4-1.7-16.9-1.7-25.5s.6-17.1 1.7-25.4l-43.3-39.4c-6.9-6.2-9.6-15.9-6.4-24.6c4.4-11.9 9.7-23.3 15.8-34.3l4.7-8.1c6.6-11 14-21.4 22.1-31.2c5.9-7.2 15.7-9.6 24.5-6.8l55.7 17.7c13.4-10.3 28.2-18.9 44-25.4l12.5-57.1c2-9.1 9-16.3 18.2-17.8C227.3 1.2 241.5 0 256 0s28.7 1.2 42.5 3.5c9.2 1.5 16.2 8.7 18.2 17.8l12.5 57.1c15.8 6.5 30.6 15.1 44 25.4l55.7-17.7c8.8-2.8 18.6-.3 24.5 6.8c8.1 9.8 15.5 20.2 22.1 31.2l4.7 8.1c6.1 11 11.4 22.4 15.8 34.3zM256 336a80 80 0 1 0 0-160a80 80 0 1 0 0 160z"
+          />
+        </svg>
+      </div>
       <thead>
         <tr class="bar_row">
           <!-- <th class="bar_item item" style="min-width: 17px">
@@ -55,7 +55,7 @@
                   bar_item_icon_down:
                     sorting.order == 'asc' && title.value === sorting.order_by,
                 }"
-                v-if="title.sortable"
+                v-if="title.sortable && title.isOrder"
               ></button>
             </div>
           </th>
@@ -77,7 +77,10 @@
             <template v-for="title in products.titles" :key="title">
               <td
                 class="item"
-                v-if="title.code === 'name'"
+                v-if="
+                  title.value === 'order_name' ||
+                  (!isTest && title.code === 'name')
+                "
                 style="padding: 5px 10px 5px 15px"
               >
                 <div class="stat">
@@ -121,17 +124,30 @@
                       statList.find((el) => el.name === row.stat)?.value !== 3
                     "
                   >
-                    {{ row.name }}
+                    {{
+                      (row.name || "") +
+                      (row.lead_id ? ` (${row.lead_id})` : "")
+                    }}
                   </a>
+                  <span v-else>
+                    {{ (row.name || "") + ` (${row.lead_id || ""})` }}
+                  </span>
                 </div>
               </td>
               <td
                 class="item"
-                v-else-if="title.code === 'stat' && row.stat === 'Успешный'"
+                v-else-if="
+                  (title.code === 'stat' || title.code === 'status') &&
+                  row.stat === 'Успешный'
+                "
               >
                 <div class="flex flex-row gap-4 items-center">
                   <span class="">
-                    {{ row[title.code] }}
+                    {{
+                      isTest
+                        ? row.fieldsForRender?.[title.value]
+                        : row[title.code]
+                    }}
                   </span>
                   <button
                     class="btn small_btn order-1 max-h-[34px] pointer-events-auto relative inline-flex whitespace-nowrap w-fit rounded-md bg-white text-[0.8125rem] font-normal leading-5 text-slate-700 shadow-sm ring-1 ring-slate-700/10 hover:bg-slate-50 hover:text-slate-900 hover:disabled:bg-white disabled:opacity-30 disabled:cursor-not-allowed"
@@ -158,7 +174,9 @@
                 :colspan="title.isGroup ? 2 : 1"
                 @click="isTest ? null : (row.isOpen = !row.isOpen)"
               >
-                {{ row[title.code] }}
+                {{
+                  isTest ? row.fieldsForRender?.[title.value] : row[title.code]
+                }}
               </td>
             </template>
           </tr>
@@ -332,8 +350,8 @@ export default {
     });
 
     const updateList = async () => {
+      await setTitles();
       await Promise.all([
-        store.dispatch("get_all_fields"),
         store.dispatch("get_account"),
         store.dispatch("getOrders", store.state.orders.filters),
       ]);
@@ -435,6 +453,22 @@ export default {
       };
       Object.assign(products?.list?.[idx]?.page, obj);
     };
+
+    // watch(
+    //   () => store.state.account.user?.table?.orders,
+    //   () => setTitles(),
+    //   { deep: true }
+    // );
+
+    const setTitles = async () => {
+      if (isTest.value) {
+        await store.dispatch("getOrdersTableConfig");
+        products.titles = computed(
+          () => store.getters.sortedFilteredTableConfig
+        );
+      }
+    };
+
     const fillOrders = () => {
       products.list = [];
       orders.value.forEach((order) => {
@@ -467,6 +501,7 @@ export default {
         if (order.company_name != null && order.contact_name != null)
           client = order.company_name + ", " + order.contact_name;
         const obj = {
+          ...order,
           id: order.id,
           page: {
             first: 1,
