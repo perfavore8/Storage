a54425738234c9c43d45e3f221bd17cc6405412e
perfavore8 <nekit2353@mail.ru>
2023-09-08 14:37:04 +0200

    feat add preporation value and product source

--- src/composables/preporationOrders.js
+++ src/composables/preporationOrders.js
@@ -1,6 +1,7 @@
 import store from "@/store";
 import { useOrdersPipelinesSelect } from "./ordersPipelinesSelect";
 import { nextTick } from "vue";
+import { computed } from "vue";
 
 export function usePreparationOrders() {
   let pipelines = null;
@@ -9,7 +10,8 @@ export function usePreparationOrders() {
     pipelines = pip;
   });
 
-  const getValue = (value, code) => value?.[code] || value?.fields?.[code];
+  const getValue = (value, code) =>
+    value?.[code] || value?.fields?.[code] || "";
 
   const dateFormater = (date) => {
     const res = new Date(date);
@@ -44,6 +46,14 @@ export function usePreparationOrders() {
           contacts.push(getValue(contact, code))
         );
         stepRes = contacts.join(", ");
+      } else if (source === "product") {
+        const contacts = [];
+        value.positions.forEach((position) => {
+          const val1 = getValue(position, code);
+          const val2 = getValue(position.product, code);
+          contacts.push(prePreparationValueByType(val1 || val2, field));
+        });
+        stepRes = contacts.join(", ");
       }
 
       stepRes = preparationValueByType(stepRes, field);
@@ -52,6 +62,39 @@ export function usePreparationOrders() {
     });
 
     return res;
+  };
+  const categories = computed(() => store.state.categories.fields_properties);
+  nextTick(() => {
+    if (!categories.value.length) store.dispatch("get_fields_properties");
+  });
+
+  const prePreparationValueByType = (value, field) => {
+    let intermediate = value;
+    if (field.type === 1 || field.type === 2) {
+      if (!value) intermediate = 0;
+      intermediate = Number(value);
+    } else if (field.type === 3 || field.type === 4) {
+      if (!value) intermediate = "";
+      intermediate = String(value);
+    } else if (field.type === 5 || field.type === 6) {
+      if (!value) intermediate = "";
+    } else if (field.type === 7 || field.type === 8) {
+      if (!value) intermediate = "";
+    } else if (field.type === 9) {
+      if (!value) intermediate = false;
+
+      if (value == "0") intermediate = false;
+      if (value == "1") {
+        intermediate = true;
+      } else {
+        intermediate = Boolean(value);
+      }
+    } else if (field.type === 12) {
+      const cat = categories.value.find((el) => el.id === value);
+      if (cat) intermediate = cat.name;
+    }
+
+    return intermediate;
   };
 
   return { preparationOrder };
