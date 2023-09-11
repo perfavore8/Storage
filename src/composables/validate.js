32c2f8825ad35aed5ebcf0ac8280651af208a134
perfavore8 <nekit2353@mail.ru>
2023-09-11 15:55:21 +0200

    new func for numbers

--- src/composables/validate.js
+++ src/composables/validate.js
@@ -4,5 +4,18 @@ export function useValidate() {
     return emailRegex.test(email);
   };
 
-  return { validateEmail };
+  function formatNumber(number) {
+    number = Math.round(number * 100) / 100;
+    let parts = number.toString().split(".");
+    parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+    if (parts.length > 1) {
+      parts[1] = parts[1].substring(0, 2);
+      parts.push(",");
+    } else {
+      parts.push(", 00");
+    }
+    return parts.join("");
+  }
+
+  return { validateEmail, formatNumber };
 }
