4892647150c245329dbee64848f993cadd7d1b00
perfavore8 <nekit2353@mail.ru>
2023-09-14 13:35:21 +0200

    refactor

--- src/composables/addToDealTabs.js
+++ src/composables/addToDealTabs.js
@@ -2,9 +2,9 @@ import { computed } from "vue";
 import { reactive } from "vue";
 
 const tabs = reactive({
-  selected: { name: "Товары", value: "products", code: "ProductsTab" },
+  selected: { name: "Заказ", value: "products", code: "ProductsTab" },
   list: [
-    { name: "Товары", value: "products", code: "ProductsTab" },
+    { name: "Заказ", value: "products", code: "ProductsTab" },
     { name: "Документы", value: "documents", code: "DocumentsTab" },
     { name: "Клиент", value: "client", code: "ClientTab" },
   ],
