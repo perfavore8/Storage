727707dccd8c895bb80b21769a1b0f5f932e6118
perfavore8 <nekit2353@mail.ru>
2023-09-18 11:38:04 +0200

    feat

---
+++ src/components/AppErrorText.vue
@@ -0,0 +1,23 @@
+<template>
+  <transition name="fade">
+    <small v-if="show" class="text-red-700 text-sm origin-top">
+      {{ text }}
+    </small>
+  </transition>
+</template>
+
+<script>
+export default { props: { text: String, show: Boolean } };
+</script>
+
+<style lang="scss" scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: all 0.1s ease-out;
+}
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+  transform: scale(0.5);
+}
+</style>
