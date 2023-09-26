d2baa049f8c3bee7b3a6337e812ba7808d2cdb88
perfavore8 <nekit2353@mail.ru>
2023-09-26 14:21:20 +0200

    done

--- src/components/Plans/PlansCard.vue
+++ src/components/Plans/PlansCard.vue
@@ -1,8 +1,30 @@
 <template>
   <div
-    class="ring-1 ring-gray-200 p-8 rounded-3xl"
-    :class="{ 'ring-indigo-600 ring-2': item.accent }"
+    class="ring-1 ring-gray-200 p-8 rounded-3xl relative"
+    :class="{
+      'ring-indigo-600 ring-2': item.accent,
+      'ring-slate-300 ring-2': item.current && !item.accent,
+    }"
   >
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="32"
+      height="32"
+      viewBox="0 0 24 24"
+      class="absolute top-3 right-3 text-green-600"
+      v-if="item.current"
+    >
+      <g fill="currentColor">
+        <path
+          d="M10.243 16.314L6 12.07l1.414-1.414l2.829 2.828l5.656-5.657l1.415 1.415l-7.071 7.07Z"
+        />
+        <path
+          fill-rule="evenodd"
+          d="M1 12C1 5.925 5.925 1 12 1s11 4.925 11 11s-4.925 11-11 11S1 18.075 1 12Zm11 9a9 9 0 1 1 0-18a9 9 0 0 1 0 18Z"
+          clip-rule="evenodd"
+        />
+      </g>
+    </svg>
     <h2
       class="text-lg text-gray-900 leading-8 font-semibold"
       :class="{ 'text-indigo-600': item.accent }"
@@ -12,12 +34,17 @@
     <p class="text-gray-600 leading-6 text-sm mt-4">
       {{ item.description }}
     </p>
-    <p class="flex gap-1 items-baseline mt-5">
+    <p class="flex gap-1 items-baseline mt-5 relative">
+      <small
+        class="absolute top-full -translate-y-0.5 left-1 text-xs text-gray-600/80 leading-6 font-semibold inline-flex"
+      >
+        {{ item.priceSmall }}
+      </small>
       <span class="text-4xl text-gray-900 tracking-tight font-bold">
         {{ item.price }}
       </span>
       <span class="text-sm text-gray-600 leading-6 font-semibold inline-flex">
-        <span>/</span>
+        <span class="mr-0.5">/</span>
         <span v-if="item.period != -1">
           {{ item.period }}
         </span>
@@ -37,15 +64,18 @@
       </span>
     </p>
     <a
-      @click="item.select()"
+      @click="item.current || item.select()"
       class="cursor-pointer ring-1 ring-inset ring-indigo-200 text-indigo-600 text-sm leading-6 font-semibold text-center py-2 px-3 rounded-md block mt-6"
       :class="{
         'bg-indigo-500 shadow-sm shadow-black/5 text-white': item.accent,
+        '!cursor-default text-slate-400': item.current && !item.accent,
+        '!cursor-default text-slate-500 bg-indigo-500/70':
+          item.current && item.accent,
       }"
     >
-      Buy plan
+      {{ $t("tarifs.buyBtn", { n: Number(item.current) + 1 }) }}
     </a>
-    <ul class="text-gray-600 text-sm leading-6 mt-8 list-none">
+    <ol class="text-gray-600 text-sm leading-6 mt-8 list-none">
       <li
         class="flex gap-3"
         v-for="advantage in item.advantages"
@@ -53,10 +83,36 @@
       >
         <svg
           xmlns="http://www.w3.org/2000/svg"
+          aria-hidden="true"
+          viewBox="0 0 20 24"
+          fill="currentColor"
+          class="text-gray-600 flex-none w-5 h-5"
+          v-if="advantage.type === 1"
+        >
+          <path
+            fill="currentColor"
+            d="M19 11H5a1 1 0 0 0 0 2h14a1 1 0 0 0 0-2Z"
+          />
+        </svg>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="20"
+          height="20"
+          viewBox="0 0 24 24"
+          v-else-if="advantage.type === 2"
+        >
+          <path
+            fill="currentColor"
+            d="M12 22q-2.075 0-3.9-.788t-3.175-2.137q-1.35-1.35-2.137-3.175T2 12q0-2.075.788-3.9t2.137-3.175q1.35-1.35 3.175-2.137T12 2q2.075 0 3.9.788t3.175 2.137q1.35 1.35 2.138 3.175T22 12q0 2.075-.788 3.9t-2.137 3.175q-1.35 1.35-3.175 2.138T12 22Zm0-2q3.35 0 5.675-2.325T20 12q0-3.35-2.325-5.675T12 4Q8.65 4 6.325 6.325T4 12q0 3.35 2.325 5.675T12 20Zm0-8Z"
+          />
+        </svg>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 20 20"
           fill="currentColor"
           aria-hidden="true"
           class="text-indigo-600 flex-none w-5 h-5"
+          v-else
         >
           <path
             fill-rule="evenodd"
@@ -64,9 +120,23 @@
             clip-rule="evenodd"
           ></path>
         </svg>
-        {{ advantage.text }}
+        <span class="inline-flex items-center gap-2">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            viewBox="0 0 650 512"
+            v-if="advantage.inf"
+          >
+            <path
+              fill="currentColor"
+              d="M0 241.1C0 161 65 96 145.1 96c38.5 0 75.4 15.3 102.6 42.5l72.3 72.2l72.2-72.2C419.5 111.3 456.4 96 494.9 96C575 96 640 161 640 241.1v29.7C640 351 575 416 494.9 416c-38.5 0-75.4-15.3-102.6-42.5L320 301.3l-72.2 72.2c-27.3 27.2-64.2 42.5-102.7 42.5C65 416 0 351 0 270.9v-29.8zM274.7 256l-72.2-72.2c-15.2-15.2-35.9-23.8-57.4-23.8c-44.8 0-81.1 36.3-81.1 81.1v29.7c0 44.8 36.3 81.1 81.1 81.1c21.5 0 42.2-8.5 57.4-23.8l72.2-72.1zm90.5 0l72.2 72.2c15.2 15.2 35.9 23.8 57.4 23.8c44.8 0 81.1-36.3 81.1-81.1v-29.8c0-44.8-36.3-81.1-81.1-81.1c-21.5 0-42.2 8.5-57.4 23.8L365.3 256z"
+            />
+          </svg>
+          {{ advantage.text }}
+        </span>
       </li>
-    </ul>
+    </ol>
   </div>
 </template>
 
--- src/components/Plans/PlansList.vue
+++ src/components/Plans/PlansList.vue
@@ -12,51 +12,89 @@
 import { computed, reactive } from "vue";
 import PlansCard from "./PlansCard.vue";
 import store from "@/store";
+import { useValidate } from "@/composables/validate";
+import { useLangConfiguration } from "@/composables/langConfiguration";
 export default {
   components: { PlansCard },
   setup() {
+    const { t } = useLangConfiguration();
+    const { dateFormater } = useValidate();
+
     const list = reactive([
       {
         id: 0,
-        accent: computed(() => store.state.account.account.tarif == 0),
-        header: "",
-        description: "The essentials to provide your best work for clients.",
-        price: "0₽",
-        period: "14 дней",
+        accent: false,
+        current: computed(() => store.state.account.account.tarif == 0),
+        header: "Демо 14",
+        description: "",
+        price: t("tarifs.price", { price: 0 }),
+        period: t("tarifs.d"),
+        priceSmall: t("tarifs.freePriceSmall"),
         advantages: [
-          { text: "5 products" },
-          { text: "Up to 1,000 subscribers" },
-          { text: "Basic analytics" },
+          { text: t("tarifs.pos"), inf: true },
+          { text: t("tarifs.zak"), inf: true },
         ],
       },
       {
         id: 3,
-        accent: computed(() => store.state.account.account.tarif == 0),
-        header: "Hobby",
-        description: "The essentials to provide your best work for clients.",
-        price: "0₽",
+        accent: false,
+        current: computed(() => store.state.account.account.tarif == 3),
+        header: "Бесплатный",
+        description: "",
+        price: t("tarifs.price", { price: 0 }),
         period: "-1",
         advantages: [
-          { text: "5 products" },
-          { text: "Up to 1,000 subscribers" },
-          { text: "Basic analytics" },
+          { text: t("tarifs.pol", { count: 1 }), type: 1 },
+          { text: t("tarifs.pos2", { count: 30 }), type: 1 },
+          { text: t("tarifs.ord", { count: 10 }), type: 1 },
         ],
       },
       {
         id: 2,
-        accent: computed(() => store.state.account.account.tarif == 2),
-        header: "Freelancer",
-        description: "The essentials to provide your best work for clients.",
-        price: "1700₽",
-        period: "month",
+        accent: true,
+        current: computed(() => store.state.account.account.tarif == 1), // <- 2
+        header: "Базовый",
+        description: "",
+        price: t("tarifs.price", { price: 1700 }),
+        period: t("tarifs.g"),
+        priceSmall: t("tarifs.evry"),
         advantages: [
-          { text: "5 products" },
-          { text: "Up to 1,000 subscribers" },
-          { text: "Basic analytics" },
-          { text: "48-hour support response time" },
+          { text: t("tarifs.pos"), inf: true },
+          { text: t("tarifs.zak"), inf: true },
+          { text: t("tarifs.imp") },
         ],
       },
     ]);
+
+    const setCurrent = async () => {
+      const current = list.find((el) => el.current);
+      if (!current) return;
+      const users = await store.dispatch("getUsersList");
+      const activeUsersCount = users.filter((user) => user.is_active).length;
+      const usersPayed = store.state.account.account?.users_payed;
+      const expiredAt = dateFormater(store.state.account.account?.expired_at, [
+        "en-GB",
+        { year: "numeric", month: "numeric", day: "numeric" },
+      ]);
+      current.advantages = [
+        {
+          text: t("tarifs.users", {
+            // eslint-disable-next-line no-irregular-whitespace
+            count: `${activeUsersCount} / ${usersPayed}`,
+          }),
+          type: 2,
+        },
+        { text: t("tarifs.exp", { date: `${expiredAt}` }), type: 2 },
+        ...current.advantages,
+      ];
+    };
+    setCurrent();
+
+    const setHeaders = async () => {
+      const tarifs = await store.dispatch("getTarifsList");
+      list.map((el) => (el.header = tarifs[el.id]));
+    };
+    setHeaders();
 
     return { list };
   },
