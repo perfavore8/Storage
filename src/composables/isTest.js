4c55f34e228dfa8501b95ac2537498b4f0f73dfb
perfavore8 <nekit2353@mail.ru>
2023-10-12 11:42:22 +0200

    chore: add test acc

--- src/composables/isTest.js
+++ src/composables/isTest.js
@@ -1,7 +1,7 @@
 import store from "@/store";
 import { computed } from "vue";
 
-const accountIds = [1, 591];
+const accountIds = [1, 591, 595];
 
 export const isTest = computed(() =>
   accountIds.includes(store.state.account?.account?.id)
