a6e7dde8eafbfa1b1c8e9391e3ed0fcd4ffabf16
perfavore8 <nekit2353@mail.ru>
2023-10-24 11:11:06 +0200

    relize

--- src/composables/isTest.js
+++ src/composables/isTest.js
@@ -2,11 +2,9 @@ import store from "@/store";
 import { computed } from "vue";
 
 const accountIds = [1, 591, 595];
-const accountIds2 = [1, 591, 595, 485];
+// const accountIds2 = [1, 591, 595, 485];
 
 export const isTest = computed(() =>
   accountIds.includes(store.state.account?.account?.id)
 );
-export const isTest2 = computed(() =>
-  accountIds2.includes(store.state.account?.account?.id)
-);
+export const isTest2 = computed(() => true);
